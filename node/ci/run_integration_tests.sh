ceaf94f92d22e5af628f1617eb9d0ca0cde4b38b
Substratum Services <33702713+substratumservices@users.noreply.github.com>
2018-03-16 17:11:41 -0400

    Maybe finally figured out the integration-test protection issue (#11)

--- node/ci/run_integration_tests.sh
+++ node/ci/run_integration_tests.sh
@@ -1,13 +1,16 @@
-#!/bin/bash -xev
+#!/bin/bash -xv
 # Copyright (c) 2017-2018, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
 
 CI_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 export RUST_BACKTRACE=full
-chmod -R 777 "$CI_DIR/../target" || echo "No target directory exists"
-if [[ ! -e "$CI_DIR/../target/release/node" ]]; then
-    echo "Can't run integration tests; must build release version first. Try ci/build.sh"
-    exit 1
+umask 000
+
+cargo build --release
+BUILD_RESULT=$?
+chmod -R 777 "$CI_DIR/../target"
+if [[ ! "$BUILD_RESULT" == "0" ]]; then
+    exit "$BUILD_RESULT"
 fi
 
-cargo test -- _integration
+cargo test --release -- _integration
