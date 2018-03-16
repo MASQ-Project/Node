599e6ead03647db9c37ee603788c457303811e10
Substratum Services <33702713+substratumservices@users.noreply.github.com>
2018-03-16 13:09:49 -0400

    Try this (#9)

--- node/ci/run_integration_tests.sh
+++ node/ci/run_integration_tests.sh
@@ -4,10 +4,9 @@
 CI_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 export RUST_BACKTRACE=full
-if [[ -e "$CI_DIR/../target/release/node" ]]; then
-    chmod -R 777 "$CI_DIR/../target/release"
-else
-    echo "Can't run integration tests; must build release version first"
+chmod -R 777 "$CI_DIR/../target" || echo "No target directory exists"
+if [[ ! -e "$CI_DIR/../target/release/node" ]]; then
+    echo "Can't run integration tests; must build release version first. Try ci/build.sh"
     exit 1
 fi
 
