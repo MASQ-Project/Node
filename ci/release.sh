6a7dcb7bfe6dc42ecdb6214bb0ed27897e0079ae
Substratum Services <33702713+substratumservices@users.noreply.github.com>
2018-03-27 09:15:18 -0400

    Sc 145 (#21)
    * SC-145 created release script that signs artifacts.

---
+++ ci/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -xev
+# Copyright (c) 2017-2018, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
+CI_DIR="$( cd "$( dirname "$0" )" && pwd )"
+PASSPHRASE="$1"
+EXECUTABLE="node"
+
+if [[ "$OSTYPE" == "msys" ]]; then
+  EXECUTABLE="$EXECUTABLE.exe"
+fi
+
+cd "$CI_DIR/../node"
+"ci/clean.sh"
+"ci/build.sh"
+
+# sign
+gpg --pinentry-mode loopback --passphrase "$PASSPHRASE" -b target/release/$EXECUTABLE
+gpg --verify target/release/$EXECUTABLE.sig target/release/$EXECUTABLE
