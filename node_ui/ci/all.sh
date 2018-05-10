5359f93793fc0821776f485e9d142e859c27affb
Substratum Services <33702713+substratumservices@users.noreply.github.com>
2018-05-10 16:12:43 -0400

    Revert "Sc 125" (#63)
    * Revert "Revert "This reverts a3f8de2de922dce36e5e9eb1368d21410fcce8a8 (SC-125) (#60)" (#62)"
    
    This reverts commit 32b0ed48a8b05fa526962285b7f08977da8d5052.
    
    * Revert "This reverts a3f8de2de922dce36e5e9eb1368d21410fcce8a8 (SC-125) (#60)"
    
    This reverts commit f70262762dd0dcffcda66fe827e6b1ac608ce200.
    
    * Revert "Sc 125 (#58)"
    
    This reverts commit a3f8de2de922dce36e5e9eb1368d21410fcce8a8.

--- node_ui/ci/all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xev
-# Copyright (c) 2017-2018, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
-CI_DIR="$( cd "$( dirname "$0" )" && pwd )"
-NODE_PARENT_DIR="$1"
-
-"$CI_DIR/setup.sh"
-"$CI_DIR/lint.sh"
-npm test
-
--- node_ui/static/scripts/substratum_node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# substratum node privilege dropper sudo hack, because electron-sudo fakes sudo on linux
-export SUDO_UID=$1
-export SUDO_GID=$2
-shift 2
-echo PID=$$
-$@ > /dev/null # ignore stdout to avoid overflowing the buffer
-
-
