69e3f7c1e8b11931b361ef3426797806e010e279
Substratum Services <33702713+substratumservices@users.noreply.github.com>
2018-05-17 09:49:33 -0400

    SC-225: Trying to make node and node_ui more OS aware (#71)

--- node/ci/integration_tests.sh
+++ node/ci/integration_tests.sh
@@ -8,18 +8,28 @@ if [[ "$NODE_PARENT_DIR" == "" ]]; then
     NODE_PARENT_DIR="$CI_DIR/../.."
 fi
 
-if [[ "$OSTYPE" == "msys" ]]; then
-    echo "Windows"
-    "$CI_DIR/run_integration_tests.sh"
-else
-    echo "Not Windows"
-    cd "$CI_DIR/../docker/integration_tests"
+case "$OSTYPE" in
+    msys)
+        echo "Windows"
+        "$CI_DIR/run_integration_tests.sh"
+        ;;
+    Darwin | darwin*)
+        echo "macOS"
+        sudo "$CI_DIR/run_integration_tests.sh"
+        ;;
+    linux-gnu)
+        echo "Linux"
+        cd "$CI_DIR/../docker/integration_tests"
 
-    echo "Path: $PATH"
-    docker build -t node_integration_test .
+        echo "Path: $PATH"
+        docker build -t node_integration_test .
 
-    echo "NODE_BASE_DIR: $NODE_PARENT_DIR"
-    echo "Contents:"
-    echo $(ls "$NODE_PARENT_DIR")
-    docker run -v "$NODE_PARENT_DIR":/node_root -t node_integration_test
-fi
+        echo "NODE_BASE_DIR: $NODE_PARENT_DIR"
+        echo "Contents:"
+        echo $(ls "$NODE_PARENT_DIR")
+        docker run -v "$NODE_PARENT_DIR":/node_root -t node_integration_test
+        ;;
+    *)
+        exit 1
+        ;;
+esac
--- node_ui/ci/integration_tests.sh
+++ node_ui/ci/integration_tests.sh
@@ -3,16 +3,24 @@
 
 CI_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-if [[ "$OSTYPE" == "msys" ]]; then
-    echo "Windows"
-    mkdir -p "$CI_DIR/../static/binaries/win"
-    ln -fs "$CI_DIR/../../node/target/release/SubstratumNode.exe" "$CI_DIR/../static/binaries/win/"
-else
-    echo "Not Windows"
-    mkdir -p "$CI_DIR/../static/binaries/linux"
-    ln -fs "$CI_DIR/../../node/target/release/SubstratumNode" "$CI_DIR/../static/binaries/linux/"
-    # When we fix the problem described in node/ci/all.sh, bring these lines back
-#    mkdir -p "$CI_DIR/../static/binaries/mac"
-#    ln -fs "$CI_DIR/../../node/target/release/SubstratumNode" "$CI_DIR/../static/binaries/mac/"
-fi
+case "$OSTYPE" in
+    msys)
+        echo "Windows"
+        mkdir -p "$CI_DIR/../static/binaries/win"
+        ln -fs "$CI_DIR/../../node/target/release/SubstratumNode.exe" "$CI_DIR/../static/binaries/win/"
+        ;;
+    Darwin | darwin*)
+        echo "macOS"
+            mkdir -p "$CI_DIR/../static/binaries/mac"
+            ln -fs "$CI_DIR/../../node/target/release/SubstratumNode" "$CI_DIR/../static/binaries/mac/"
+        ;;
+    linux-gnu)
+        echo "Linux"
+        mkdir -p "$CI_DIR/../static/binaries/linux"
+        ln -fs "$CI_DIR/../../node/target/release/SubstratumNode" "$CI_DIR/../static/binaries/linux/"
+        ;;
+    *)
+        exit 1
+        ;;
+esac
 yarn spec
