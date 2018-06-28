a137391be159404c6d4bdf520940c43a182a64ed
Substratum Services <33702713+substratumservices@users.noreply.github.com>
2018-06-28 15:14:36 -0400

    SC-244 Push button SubstratumNode binary deployments to S3 (#112)
    * SC-244 Build release artifact.
    
    Adding logic to build the release artifact and the sha hash.
    
    Added --batch to gpg for better scripting. Will not prompt for
    overwrite and returns proper exit code when file already exists.
    
    Ensure dns_utility.exe is included in Windows binary distribution.
    
    Remove --pinentry-mode used in windows replaced
    by --batch to suppress interactive prompts.
    
    Removing shasum from the release flow and moving it into the jenkins
    Distribute-Release-Artifact job.

--- ci/release.sh
+++ ci/release.sh
@@ -2,10 +2,12 @@
 # Copyright (c) 2017-2018, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
 CI_DIR="$( cd "$( dirname "$0" )" && pwd )"
 PASSPHRASE="$1"
-EXECUTABLE="SubstratumNode"
+NODE_EXECUTABLE="SubstratumNode"
+DNS_EXECUTABLE="dns_utility"
 
 if [[ "$OSTYPE" == "msys" ]]; then
-  EXECUTABLE="$EXECUTABLE.exe"
+  NODE_EXECUTABLE="$NODE_EXECUTABLE.exe"
+  DNS_EXECUTABLE="$DNS_EXECUTABLE.exe"
 fi
 
 cd "$CI_DIR/../dns_utility"
@@ -17,9 +19,29 @@ cargo clean
 "ci/build.sh"
 
 # sign
-gpg --pinentry-mode loopback --passphrase "$PASSPHRASE" -b target/release/$EXECUTABLE
-gpg --verify target/release/$EXECUTABLE.sig target/release/$EXECUTABLE
+gpg --batch --passphrase "$PASSPHRASE" -b target/release/$NODE_EXECUTABLE
+gpg --verify target/release/$NODE_EXECUTABLE.sig target/release/$NODE_EXECUTABLE
 
 # gui
 cd "$CI_DIR/../node_ui"
 "ci/release.sh"
+
+cd "$CI_DIR/../"
+
+case "$OSTYPE" in
+   linux*)
+        zip -j SubstratumNode-Linux64-binary.zip dns_utility/target/release/$DNS_EXECUTABLE node/target/release/$NODE_EXECUTABLE node/target/release/$NODE_EXECUTABLE.sig
+        zip -j SubstratumNode-Linux64-deb.zip node_ui/dist/SubstratumNode*.deb
+        ;;
+   darwin*)
+        zip -j SubstratumNode-macOS-binary.zip dns_utility/target/release/$DNS_EXECUTABLE node/target/release/$NODE_EXECUTABLE node/target/release/$NODE_EXECUTABLE.sig
+        zip -j SubstratumNode-macOS.dmg.zip node_ui/dist/SubstratumNode*.dmg
+        ;;
+   msys)
+        zip -j SubstratumNode-Windows-binary.zip dns_utility/target/release/$DNS_EXECUTABLE node/target/release/$NODE_EXECUTABLE node/target/release/$NODE_EXECUTABLE.sig
+        zip -j SubstratumNode-Windows.exe.zip node_ui/dist/SubstratumNode*.exe
+        ;;
+   *)
+        echo "unsupported operating system detected."; exit 1
+        ;;
+esac
