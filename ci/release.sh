ddab2ec8feec7ff69fd09094a17f2217054d954e
Substratum Services <33702713+substratumservices@users.noreply.github.com>
2019-03-27 15:18:19 -0400

    SC-777: Addressed issues with release script (#319)
    https://github.com/SubstratumNetwork/SubstratumNode/pull/13:
    Install NPM dependencies in correct order

--- ci/release.sh
+++ ci/release.sh
@@ -33,15 +33,15 @@ cd "$CI_DIR/../"
 case "$OSTYPE" in
    linux*)
         zip -j SubstratumNode-Linux64-binary.zip dns_utility/target/release/$DNS_EXECUTABLE node/target/release/$NODE_EXECUTABLE node/target/release/$NODE_EXECUTABLE.sig
-        zip -j SubstratumNode-Linux64-deb.zip node-ui/dist/SubstratumNode*.deb
+        zip -j SubstratumNode-Linux64-deb.zip node-ui/electron-builder-out/SubstratumNode*.deb
         ;;
    darwin*)
         zip -j SubstratumNode-macOS-binary.zip dns_utility/target/release/$DNS_EXECUTABLE node/target/release/$NODE_EXECUTABLE node/target/release/$NODE_EXECUTABLE.sig
-        zip -j SubstratumNode-macOS.dmg.zip node-ui/dist/SubstratumNode*.dmg
+        zip -j SubstratumNode-macOS.dmg.zip node-ui/electron-builder-out/SubstratumNode*.dmg
         ;;
    msys)
         zip -j SubstratumNode-Windows-binary.zip dns_utility/target/release/$DNS_EXECUTABLE node/target/release/$NODE_EXECUTABLE node/target/release/$NODE_EXECUTABLE.sig
-        zip -j SubstratumNode-Windows.exe.zip node-ui/dist/SubstratumNode*.exe
+        zip -j SubstratumNode-Windows.exe.zip node-ui/electron-builder-out/SubstratumNode*.exe
         ;;
    *)
         echo "unsupported operating system detected."; exit 1
