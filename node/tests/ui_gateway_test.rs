38d33136435933004da21ffe161a4dca39d72aaa
Dan Wiebe <dnwiebe@gmail.com>
2019-12-09 14:15:26 -0500

    GH-140: Cleaned up some integration tests

--- node/tests/ui_gateway_test.rs
+++ node/tests/ui_gateway_test.rs
@@ -9,7 +9,6 @@ use node_lib::test_utils::assert_matches;
 use node_lib::ui_gateway::ui_traffic_converter::{UiTrafficConverter, UiTrafficConverterReal};
 use std::time::Duration;
 use tokio::prelude::*;
-use tokio::runtime::Runtime;
 use websocket::ClientBuilder;
 use websocket::OwnedMessage;
 use node_lib::sub_lib::accountant::{UiFinancialsResponse, UiFinancialsRequest};
@@ -24,7 +23,7 @@ fn ui_gateway_message_integration() {
         .marshal(UiMessage::GetNodeDescriptor)
         .expect("Couldn't marshal GetNodeDescriptor message");
 
-    let descriptor_client =
+    let _ =
         ClientBuilder::new(format!("ws://{}:{}", localhost(), DEFAULT_UI_PORT).as_str())
             .expect("Couldn't create first ClientBuilder")
             .add_protocol("MASQNode-UI")
@@ -38,26 +37,7 @@ fn ui_gateway_message_integration() {
             .timeout(Duration::from_millis(1000))
             .map_err(|e| panic!("failed to get response by timeout {:?}", e));
 
-    let shutdown_msg = converter
-        .marshal(UiMessage::ShutdownMessage)
-        .expect("Couldn't marshal ShutdownMessage");
-
-    let shutdown_client =
-        ClientBuilder::new(format!("ws://{}:{}", localhost(), DEFAULT_UI_PORT).as_str())
-            .expect("Couldn't create second ClientBuilder")
-            .add_protocol("MASQNode-UI")
-            .async_connect_insecure()
-            .and_then(|(s, _)| s.send(OwnedMessage::Text(shutdown_msg)));
-
-    let mut rt = Runtime::new().expect("Couldn't create Runtime");
-    rt.block_on(descriptor_client)
-        .expect("Couldn't block on descriptor_client");
-    rt.block_on(shutdown_client)
-        .expect("Couldn't block on shutdown_client");
-    rt.shutdown_on_idle()
-        .wait()
-        .expect("Couldn't wait on shutdown_on_idle");
-
+    node.kill().unwrap();
     node.wait_for_exit();
 }
 
@@ -72,37 +52,23 @@ fn ui_gateway_dot_graph_message_integration() {
         .marshal(UiMessage::NeighborhoodDotGraphRequest)
         .unwrap();
 
-    let digraph_client =
-        ClientBuilder::new(format!("ws://{}:{}", localhost(), DEFAULT_UI_PORT).as_str())
-            .unwrap()
-            .add_protocol("MASQNode-UI")
-            .async_connect_insecure()
-            .and_then(|(s, _)| s.send(OwnedMessage::Text(msg)))
-            .and_then(|s| s.into_future().map_err(|e| e.0))
-            .map(|(m, _)| match m {
-                Some(OwnedMessage::Text(s)) => {
-                    assert!(!s.is_empty());
-                    assert_matches(s.as_str(), r"digraph db \{ .+ \}");
-                }
-                _ => panic!("Expected a text response"),
-            })
-            .timeout(Duration::from_millis(2000))
-            .map_err(|e| panic!("failed to get response by timeout {:?}", e));
+    let _ = ClientBuilder::new(format!("ws://{}:{}", localhost(), DEFAULT_UI_PORT).as_str())
+        .unwrap()
+        .add_protocol("MASQNode-UI")
+        .async_connect_insecure()
+        .and_then(|(s, _)| s.send(OwnedMessage::Text(msg)))
+        .and_then(|s| s.into_future().map_err(|e| e.0))
+        .map(|(m, _)| match m {
+            Some(OwnedMessage::Text(s)) => {
+                assert!(!s.is_empty());
+                assert_matches(s.as_str(), r"digraph db \{ .+ \}");
+            }
+            _ => panic!("Expected a text response"),
+        })
+        .timeout(Duration::from_millis(2000))
+        .map_err(|e| panic!("failed to get response by timeout {:?}", e));
 
-    let shutdown_msg = converter.marshal(UiMessage::ShutdownMessage).unwrap();
-
-    let shutdown_client =
-        ClientBuilder::new(format!("ws://{}:{}", localhost(), DEFAULT_UI_PORT).as_str())
-            .unwrap()
-            .add_protocol("MASQNode-UI")
-            .async_connect_insecure()
-            .and_then(|(s, _)| s.send(OwnedMessage::Text(shutdown_msg)));
-
-    let mut rt = Runtime::new().unwrap();
-    rt.block_on(digraph_client).unwrap();
-    rt.block_on(shutdown_client).unwrap();
-    rt.shutdown_on_idle().wait().unwrap();
-
+    node.kill().unwrap();
     node.wait_for_exit();
 }
 
@@ -126,41 +92,27 @@ fn request_financial_information_integration() {
         payload: serde_json::to_string(&payload).unwrap(),
     });
 
-    let digraph_client =
-        ClientBuilder::new(format!("ws://{}:{}", localhost(), DEFAULT_UI_PORT).as_str())
-            .unwrap()
-            .add_protocol("MASQNode-UIv2")
-            .async_connect_insecure()
-            .and_then(|(s, _)| s.send(OwnedMessage::Text(request_msg)))
-            .and_then(|s| s.into_future().map_err(|e| e.0))
-            .map(|(m, _)| match m {
-                Some (OwnedMessage::Text(response_json)) => {
-                    let response_msg: NewUiMessage = UiTrafficConverterReal::new().new_unmarshal(&response_json, 1234).unwrap();
-                    assert_eq!(response_msg.opcode, "financials".to_string());
-                    assert_eq!(response_msg.client_id, 1234);
-                    assert_eq!(response_msg.direction, MessageDirection::ToUi);
-                    let payload = serde_json::from_str::<UiFinancialsResponse>(&response_msg.payload).unwrap();
-                    assert_eq!(payload.payables.len(), 0);
-                    assert_eq!(payload.receivables.len(), 0);
-                },
-                other => panic!("Expected text, received {:?}", other),
-            })
-            .timeout(Duration::from_millis(2000))
-            .map_err(|e| panic!("failed to get response by timeout {:?}", e));
+    let _ = ClientBuilder::new(format!("ws://{}:{}", localhost(), DEFAULT_UI_PORT).as_str())
+        .unwrap()
+        .add_protocol("MASQNode-UIv2")
+        .async_connect_insecure()
+        .and_then(|(s, _)| s.send(OwnedMessage::Text(request_msg)))
+        .and_then(|s| s.into_future().map_err(|e| e.0))
+        .map(|(m, _)| match m {
+            Some (OwnedMessage::Text(response_json)) => {
+                let response_msg: NewUiMessage = UiTrafficConverterReal::new().new_unmarshal(&response_json, 1234).unwrap();
+                assert_eq!(response_msg.opcode, "financials".to_string());
+                assert_eq!(response_msg.client_id, 1234);
+                assert_eq!(response_msg.direction, MessageDirection::ToUi);
+                let payload = serde_json::from_str::<UiFinancialsResponse>(&response_msg.payload).unwrap();
+                assert_eq!(payload.payables.len(), 0);
+                assert_eq!(payload.receivables.len(), 0);
+            },
+            other => panic!("Expected text, received {:?}", other),
+        })
+        .timeout(Duration::from_millis(2000))
+        .map_err(|e| panic!("failed to get response by timeout {:?}", e));
 
-    let shutdown_msg = converter.marshal(UiMessage::ShutdownMessage).unwrap();
-
-    let shutdown_client =
-        ClientBuilder::new(format!("ws://{}:{}", localhost(), DEFAULT_UI_PORT).as_str())
-            .unwrap()
-            .add_protocol("MASQNode-UI")
-            .async_connect_insecure()
-            .and_then(|(s, _)| s.send(OwnedMessage::Text(shutdown_msg)));
-
-    let mut rt = Runtime::new().unwrap();
-    rt.block_on(digraph_client).unwrap();
-    rt.block_on(shutdown_client).unwrap();
-    rt.shutdown_on_idle().wait().unwrap();
-
+    node.kill().unwrap();
     node.wait_for_exit();
 }
