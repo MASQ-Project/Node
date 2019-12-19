23bcd907a05b4a89a26370915e286ec63ee51fb3
Dan Wiebe <dnwiebe@gmail.com>
2019-12-19 18:40:49 -0500

    GH-242: Now the receipt of a UiStartOrder message calls the Launcher (which doesn't exist yet except as a mock) properly

--- node/src/daemon.rs
+++ node/src/daemon.rs
@@ -8,6 +8,8 @@ use crate::sub_lib::ui_gateway::MessageTarget::ClientId;
 use crate::sub_lib::logger::Logger;
 use std::collections::HashMap;
 use itertools::Itertools;
+use crate::sub_lib::neighborhood::NodeDescriptor;
+use std::path::PathBuf;
 
 #[derive(Message, PartialEq, Clone)]
 pub struct DaemonBindMessage {
@@ -16,14 +18,25 @@ pub struct DaemonBindMessage {
     pub from_ui_message_recipients: Vec<Recipient<NodeFromUiMessage>>, // for the UiGateway to relay inbound UI messages to everybody
 }
 
+struct LaunchSuccess {
+    pub descriptor: NodeDescriptor,
+    pub log_file: PathBuf,
+    pub new_process_id: i32,
+    pub redirect_ui_port: u16,
+}
+
+enum LaunchError {
+
+}
+
 trait Launcher {
-    fn launch(self, params: Vec<String>) -> String;
+    fn launch(self, params: Vec<String>) -> Result<LaunchSuccess, LaunchError>;
 }
 
 struct LauncherReal {}
 
 impl Launcher for LauncherReal {
-    fn launch(self, _params: Vec<String>) -> String {
+    fn launch(self, _params: Vec<String>) -> Result<LaunchSuccess, LaunchError> {
         unimplemented!()
     }
 }
@@ -35,7 +48,8 @@ impl LauncherReal {
 }
 
 pub struct Daemon {
-    _launcher: Box<dyn Launcher>,
+    launcher: Box<dyn Launcher>,
+    params: HashMap<String, String>,
     ui_gateway_sub: Option<Recipient<NodeToUiMessage>>,
     logger: Logger,
 }
@@ -68,16 +82,8 @@ impl Handler<NodeFromUiMessage> for Daemon {
 
 impl Daemon {
     pub fn new() -> Daemon {
-        Daemon {
-            _launcher: Box::new(LauncherReal::new()),
-            ui_gateway_sub: None,
-            logger: Logger::new("Daemon"),
-        }
-    }
-
-    fn handle_setup(&mut self, client_id: u64, context_id: u64, payload: UiSetup) {
         let schema = crate::node_configurator::node_configurator_standard::app();
-        let mut pairs: HashMap<String, String> = schema.p.opts
+        let mut params: HashMap<String, String> = schema.p.opts
             .iter()
             .flat_map(|opt| {
                 let name = opt.b.name.to_string();
@@ -87,14 +93,23 @@ impl Daemon {
                 }
             })
             .collect();
-        pairs.insert ("dns-servers".to_string(), "1.1.1.1".to_string()); // TODO: This should default to the system DNS value before subversion.
+        params.insert ("dns-servers".to_string(), "1.1.1.1".to_string()); // TODO: This should default to the system DNS value before subversion.
+        Daemon {
+            launcher: Box::new(LauncherReal::new()),
+            params,
+            ui_gateway_sub: None,
+            logger: Logger::new("Daemon"),
+        }
+    }
+
+    fn handle_setup(&mut self, client_id: u64, context_id: u64, payload: UiSetup) {
         payload.values
             .into_iter ()
             .for_each(|value| {
-                pairs.insert (value.name, value.value);
+                self.params.insert (value.name, value.value);
             });
-        let ui_setup_values = pairs.keys().map(|key| {
-                let value = pairs.get(key).expect ("Value disappeared!");
+        let ui_setup_values = self.params.keys().map(|key| {
+                let value = self.params.get(key).expect ("Value disappeared!");
                 UiSetupValue {name: key.clone(), value: value.clone()}
             })
             .sorted_by_key (|ui_setup_value| {ui_setup_value.name.clone()})
@@ -118,36 +133,37 @@ mod tests {
     use crate::test_utils::recorder::{make_recorder, Recorder};
     use actix::System;
     use crate::sub_lib::ui_gateway::MessageTarget::ClientId;
-    use crate::ui_gateway::messages::{UiSetup};
+    use crate::ui_gateway::messages::{UiSetup, UiStartOrder, UiStartResponse};
     use std::collections::HashSet;
+    use crate::test_utils::main_cryptde;
 
     struct LauncherMock {
         launch_params: Arc<Mutex<Vec<Vec<String>>>>,
-        launch_results: RefCell<Vec<String>>,
+        launch_results: RefCell<Vec<Result<LaunchSuccess, LaunchError>>>,
     }
 
     impl Launcher for LauncherMock {
-        fn launch(self, params: Vec<String>) -> String {
+        fn launch(self, params: Vec<String>) -> Result<LaunchSuccess, LaunchError> {
             self.launch_params.lock().unwrap().push (params);
             self.launch_results.borrow_mut().remove(0)
         }
     }
 
     impl LauncherMock {
-        fn _new() -> LauncherMock {
+        fn new() -> LauncherMock {
             LauncherMock {
                 launch_params: Arc::new(Mutex::new(vec![])),
                 launch_results: RefCell::new(vec![]),
             }
         }
 
-        fn _launch_params(mut self, params: &Arc<Mutex<Vec<Vec<String>>>>) -> Self {
+        fn launch_params(mut self, params: &Arc<Mutex<Vec<Vec<String>>>>) -> Self {
             self.launch_params = params.clone();
             self
         }
 
-        fn _launch_result(self, result: &str) -> Self {
-            self.launch_results.borrow_mut().push (result.to_string());
+        fn launch_result(self, result: Result<LaunchSuccess, LaunchError>) -> Self {
+            self.launch_results.borrow_mut().push (result);
             self
         }
     }
@@ -203,7 +219,7 @@ mod tests {
     }
 
     #[test]
-    fn accepts_full_setup_and_returns_settings() {
+    fn accepts_full_setup_and_returns_settings_then_remembers_them() {
         let (ui_gateway, _, ui_gateway_recording_arc) = make_recorder();
         let system = System::new("test");
         let subject = Daemon::new();
@@ -218,6 +234,10 @@ mod tests {
                 UiSetupValue::new("db-password", "goober"),
                 UiSetupValue::new("real-user", "1234:4321:hormel"),
             ]}.tmb(4321),
+        }).unwrap();
+        subject_addr.try_send(NodeFromUiMessage {
+            client_id: 1234,
+            body: UiSetup{values: vec![]}.tmb(4321),
         }).unwrap();
 
         System::current().stop();
@@ -248,5 +268,60 @@ mod tests {
         expected_pairs.insert ("real-user".to_string(), "1234:4321:hormel".to_string());
 
         assert_eq! (actual_pairs, expected_pairs);
+
+        let record = ui_gateway_recording.get_record::<NodeToUiMessage>(1).clone();
+        assert_eq! (record.target, ClientId(1234));
+        let (payload, context_id): (UiSetup, u64) = UiSetup::fmb(record.body).unwrap();
+        assert_eq! (context_id, 4321);
+        let actual_pairs: HashMap<String, String> = payload.values.into_iter()
+            .map(|value| (value.name, value.value))
+            .collect();
+
+        assert_eq! (actual_pairs, expected_pairs);
+    }
+
+    #[test]
+    fn accepts_start_order_and_calls_launcher() {
+        let main_cryptde = main_cryptde();
+        let (ui_gateway, _, ui_gateway_recording_arc) = make_recorder();
+        let launch_params_arc = Arc::new (Mutex::new (vec![]));
+        let launcher = LauncherMock::new()
+            .launch_params (&launch_params_arc)
+            .launch_result (Ok(LaunchSuccess {
+                descriptor: NodeDescriptor::from_str(main_cryptde, "Rmxvb2JsZXM@1.2.3.4:1234").unwrap(),
+                log_file: PathBuf::from ("booga"),
+                new_process_id: 1234,
+                redirect_ui_port: 2345
+            }));
+        let system = System::new("test");
+        let mut subject = Daemon::new();
+        subject.launcher = Box::new (launcher);
+        subject.params.insert ("db-password".to_string(), "goober".to_string());
+        let subject_addr = subject.start();
+        subject_addr.try_send(make_bind_message(ui_gateway)).unwrap();
+
+        subject_addr.try_send(NodeFromUiMessage {
+            client_id: 1234,
+            body: UiStartOrder{}.tmb(4321),
+        }).unwrap();
+
+        System::current().stop();
+        system.run();
+        let ui_gateway_recording = ui_gateway_recording_arc.lock().unwrap();
+        let record = ui_gateway_recording.get_record::<NodeToUiMessage>(0).clone();
+        assert_eq! (record.target, ClientId(1234));
+        let (payload, context_id): (UiStartResponse, u64) = UiStartResponse::fmb(record.body).unwrap();
+        assert_eq! (context_id, 4321);
+        assert_eq! (payload, UiStartResponse {
+            descriptor: "Rmxvb2JsZXM@1.2.3.4:1234".to_string(),
+            log_file: "booga".to_string(),
+            new_process_id: 1234,
+            redirect_ui_port: 2345
+        });
+        let launch_params = launch_params_arc.lock().unwrap();
+        assert_eq! (*launch_params, vec![vec![
+            "db-password".to_string(), "goober".to_string(),
+            "dns-servers".to_string(), "1.1.1.1".to_string(),
+        ]])
     }
 }
