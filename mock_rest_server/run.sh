2e63d5e080e9934ec4754b047f70b05a49876187
Dan Wiebe <dnwiebe@gmail.com>
2020-07-06 21:46:20 -0400

    GH-309: Removed mock_rest_server project

--- mock_rest_server/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
-docker run -p 8000:8000 -p 80:80 mock_rest_server
