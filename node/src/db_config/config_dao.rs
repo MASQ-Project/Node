3303bdf5d192f3374943f745b4885cf3807d9ecc
Dan Wiebe <dnwiebe@gmail.com>
2020-10-29 08:30:28 -0400

    GH-325: Good start on ConfigDao

--- node/src/db_config/config_dao.rs
+++ node/src/db_config/config_dao.rs
@@ -5,7 +5,7 @@ use crate::database::db_initializer::ConnectionWrapper;
 use crate::sub_lib::cryptde::PlainData;
 use rand::Rng;
 use rusqlite::types::ToSql;
-use rusqlite::{OptionalExtension, Rows, Transaction, NO_PARAMS};
+use rusqlite::{OptionalExtension, Rows, Transaction, NO_PARAMS, Row};
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum ConfigDaoError {
@@ -66,13 +66,80 @@ pub struct ConfigDaoReal {
     conn: Box<dyn ConnectionWrapper>,
 }
 
+impl ConfigDao for ConfigDaoReal {
+    fn get_all(&self) -> Result<Vec<ConfigDaoRecord>, ConfigDaoError> {
+        let mut stmt = self
+            .conn
+            .prepare("select name, value, encrypted from config")
+            .expect("Schema error: couldn't compose query for config table");
+        let mut rows: Rows = stmt
+            .query(NO_PARAMS)
+            .expect("Schema error: couldn't dump config table");
+        let mut results = vec![];
+        loop {
+            match rows.next() {
+                Err(e) => unimplemented!(),
+                Ok(Some(row)) => results.push (Self::row_to_config_dao_record(row)),
+                Ok(None) => break,
+            }
+        }
+        Ok(results)
+    }
+
+    fn get(&self, name: &str) -> Result<ConfigDaoRecord, ConfigDaoError> {
+        let mut stmt = match self.conn.prepare("select name, value, encrypted from config where name = ?") {
+            Ok(stmt) => stmt,
+            Err(e) => return Err(ConfigDaoError::DatabaseError(format!("{}", e))),
+        };
+        match stmt.query_row(&[name], |row| Ok(Self::row_to_config_dao_record(row))) {
+            Ok(record) => Ok(record),
+            Err(rusqlite::Error::QueryReturnedNoRows) => Err(ConfigDaoError::NotPresent),
+            // The following line is untested, because we don't know how to trigger it.
+            Err(e) => Err(ConfigDaoError::DatabaseError(format!("{}", e))),
+        }
+    }
+
+    fn transaction(&self) -> Box<dyn TransactionWrapper> {
+        unimplemented!()
+    }
+
+    fn set(&self, name: &str, value: Option<&str>) -> Result<(), ConfigDaoError> {
+        let mut stmt = match self
+            .conn
+            .prepare("update config set value = ? where name = ?")
+        {
+            Ok(stmt) => stmt,
+            Err(e) => unimplemented!(), //return Err(ConfigDaoError::DatabaseError(format!("{}", e))),
+        };
+        let params: &[&dyn ToSql] = &[&value, &name];
+        Self::handle_update_execution(stmt.execute(params))
+    }
+}
+
 impl ConfigDaoReal {
     pub fn new(conn: Box<dyn ConnectionWrapper>) -> ConfigDaoReal {
         ConfigDaoReal { conn }
     }
+
+    fn row_to_config_dao_record(row: &Row) -> ConfigDaoRecord {
+        let name: String = row.get(0).expect("Schema error: no name column");
+        let value_opt: Option<String> = row.get(1).expect("Schema error: no value column");
+        let encrypted_int: i32 = row.get(2).expect("Schema error: no encrypted column");
+        match value_opt {
+            Some (value) => ConfigDaoRecord::new(&name, Some (&value), encrypted_int != 0),
+            None => ConfigDaoRecord::new (&name, None, encrypted_int != 0),
+        }
+    }
+
+    fn handle_update_execution(result: rusqlite::Result<usize>) -> Result<(), ConfigDaoError> {
+        match result {
+            Ok(0) => Err(ConfigDaoError::NotPresent),
+            Ok(_) => Ok(()),
+            Err(e) => unimplemented!(), //Err(ConfigDaoError::DatabaseError(format!("{}", e))), // Don't know how to trigger this
+        }
+    }
 }
 
-/*
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -87,236 +154,82 @@ mod tests {
     #[test]
     fn get_all_returns_multiple_results() {
         let home_dir =
-            ensure_node_home_directory_exists("node", "get_all_returns_multiple_results");
+            ensure_node_home_directory_exists("config_dao", "get_all_returns_multiple_results");
         let subject = ConfigDaoReal::new(
             DbInitializerReal::new()
                 .initialize(&home_dir, DEFAULT_CHAIN_ID, true)
                 .unwrap(),
         );
 
-        let result = subject.get_all(None).unwrap();
+        let result = subject.get_all().unwrap();
 
         assert_contains(
             &result,
-            &(
-                "schema_version".to_string(),
-                Some(CURRENT_SCHEMA_VERSION.to_string()),
-            ),
+            &ConfigDaoRecord::new("schema_version", Some(CURRENT_SCHEMA_VERSION), false),
         );
         assert_contains(
             &result,
-            &(
-                "start_block".to_string(),
-                Some(ROPSTEN_TESTNET_CONTRACT_CREATION_BLOCK.to_string()),
-            ),
+            &ConfigDaoRecord::new("start_block", Some(&ROPSTEN_TESTNET_CONTRACT_CREATION_BLOCK.to_string()), false),
         );
-        assert_contains(&result, &("seed".to_string(), None));
+        assert_contains(&result, &ConfigDaoRecord::new ("seed", None, true));
     }
 
     #[test]
-    fn clear_removes_value_but_not_row() {
-        let home_dir = ensure_node_home_directory_exists("node", "clear_removes_value_but_not_row");
+    fn get_returns_not_present_if_row_doesnt_exist() {
+        let home_dir =
+            ensure_node_home_directory_exists("config_dao", "get_returns_not_present_if_row_doesnt_exist");
         let subject = ConfigDaoReal::new(
             DbInitializerReal::new()
                 .initialize(&home_dir, DEFAULT_CHAIN_ID, true)
                 .unwrap(),
         );
 
-        let _ = subject.clear("schema_version").unwrap();
+        let result = subject.get("booga");
 
-        let result = subject.get_string("schema_version");
+        assert_eq! (result, Err(ConfigDaoError::NotPresent));
+    }
+
+    #[test]
+    fn set_and_get_work() {
+        let home_dir = ensure_node_home_directory_exists("config_dao", "set_and_get_work");
+        let subject = ConfigDaoReal::new(
+            DbInitializerReal::new()
+                .initialize(&home_dir, DEFAULT_CHAIN_ID, true)
+                .unwrap(),
+        );
+
+        let _ = subject.set("seed", Some ("Two wrongs don't make a right, but two Wrights make an airplane")).unwrap();
+
+        let result = subject.get("seed").unwrap();
+        assert_eq!(result, ConfigDaoRecord::new ("seed", Some("Two wrongs don't make a right, but two Wrights make an airplane"), true));
+    }
+
+    #[test]
+    fn setting_nonexistent_value_returns_not_present() {
+        let home_dir = ensure_node_home_directory_exists("config_dao", "setting_nonexistent_value_returns_not_present");
+        let subject = ConfigDaoReal::new(
+            DbInitializerReal::new()
+                .initialize(&home_dir, DEFAULT_CHAIN_ID, true)
+                .unwrap(),
+        );
+
+        let result = subject.set("booga", Some ("bigglesworth"));
+
         assert_eq!(result, Err(ConfigDaoError::NotPresent));
     }
 
     #[test]
-    fn check_password_handles_missing_password() {
-        let home_dir =
-            ensure_node_home_directory_exists("node", "check_password_handles_missing_password");
+    fn setting_value_to_none_removes_value_but_not_row() {
+        let home_dir = ensure_node_home_directory_exists("config_dao", "setting_value_to_none_removes_value_but_not_row");
         let subject = ConfigDaoReal::new(
             DbInitializerReal::new()
                 .initialize(&home_dir, DEFAULT_CHAIN_ID, true)
                 .unwrap(),
         );
 
-        let result = subject.check_password("password");
+        let _ = subject.set("schema_version", None).unwrap();
 
-        assert_eq!(result, Err(ConfigDaoError::NotPresent));
-    }
-
-    #[test]
-    fn check_password_handles_bad_password() {
-        let home_dir =
-            ensure_node_home_directory_exists("node", "check_password_handles_bad_password");
-        let subject = ConfigDaoReal::new(
-            DbInitializerReal::new()
-                .initialize(&home_dir, DEFAULT_CHAIN_ID, true)
-                .unwrap(),
-        );
-        subject.change_password(None, "password").unwrap();
-
-        let result = subject.check_password("drowssap");
-
-        assert_eq!(result, Ok(false));
-    }
-
-    #[test]
-    fn check_password_handles_good_password() {
-        let home_dir =
-            ensure_node_home_directory_exists("node", "check_password_handles_good_password");
-        let subject = ConfigDaoReal::new(
-            DbInitializerReal::new()
-                .initialize(&home_dir, DEFAULT_CHAIN_ID, true)
-                .unwrap(),
-        );
-        subject.change_password(None, "password").unwrap();
-
-        let result = subject.check_password("password");
-
-        assert_eq!(result, Ok(true));
-    }
-
-    #[test]
-    fn setting_the_first_encrypted_field_sets_the_password() {
-        let home_dir = ensure_node_home_directory_exists(
-            "node",
-            "setting_the_first_encrypted_field_sets_the_password",
-        );
-        let subject = ConfigDaoReal::new(
-            DbInitializerReal::new()
-                .initialize(&home_dir, DEFAULT_CHAIN_ID, true)
-                .unwrap(),
-        );
-        subject.add_string_rows(vec![("first_encrypted_field", true)]);
-
-        subject
-            .set_bytes_e(
-                "first_encrypted_field",
-                &PlainData::new(b"value"),
-                "password",
-            )
-            .unwrap();
-
-        assert!(subject.check_password("password").unwrap());
-    }
-
-    #[test]
-    fn change_password_complains_if_given_no_old_password_when_an_old_password_exists() {
-        let home_dir = ensure_node_home_directory_exists(
-            "node",
-            "change_password_complains_if_given_no_old_password_when_an_old_password_exists",
-        );
-        let subject = ConfigDaoReal::new(
-            DbInitializerReal::new()
-                .initialize(&home_dir, DEFAULT_CHAIN_ID, true)
-                .unwrap(),
-        );
-        let old_password = "old password";
-        let new_password = "new password";
-        subject.change_password(None, old_password).unwrap();
-
-        let result = subject.change_password(None, new_password);
-
-        assert_eq!(result, Err(ConfigDaoError::PasswordError));
-    }
-
-    #[test]
-    fn change_password_complains_if_given_old_password_when_no_old_password_exists() {
-        let home_dir = ensure_node_home_directory_exists(
-            "node",
-            "change_password_complains_if_given_old_password_when_no_old_password_exists",
-        );
-        let subject = ConfigDaoReal::new(
-            DbInitializerReal::new()
-                .initialize(&home_dir, DEFAULT_CHAIN_ID, true)
-                .unwrap(),
-        );
-        let old_password = "old password";
-        let new_password = "new password";
-
-        let result = subject.change_password(Some(old_password), new_password);
-
-        assert_eq!(result, Err(ConfigDaoError::PasswordError));
-    }
-
-    #[test]
-    fn change_password_complains_if_given_incorrect_old_password() {
-        let home_dir = ensure_node_home_directory_exists(
-            "node",
-            "change_password_complains_if_given_incorrect_old_password",
-        );
-        let subject = ConfigDaoReal::new(
-            DbInitializerReal::new()
-                .initialize(&home_dir, DEFAULT_CHAIN_ID, true)
-                .unwrap(),
-        );
-        let old_password = "old password";
-        let new_password = "new password";
-        subject.change_password(None, old_password).unwrap();
-
-        let result = subject.change_password(Some(new_password), new_password);
-
-        assert_eq!(result, Err(ConfigDaoError::PasswordError));
-    }
-
-    #[test]
-    fn change_password_works_when_old_password_exists() {
-        let home_dir = ensure_node_home_directory_exists(
-            "node",
-            "change_password_works_when_old_password_exists",
-        );
-        let subject = ConfigDaoReal::new(
-            DbInitializerReal::new()
-                .initialize(&home_dir, DEFAULT_CHAIN_ID, true)
-                .unwrap(),
-        );
-        let old_password = "old password";
-        let new_password = "new password";
-        let cleartext_one = "cleartext one";
-        let cleartext_two = "cleartext two";
-        let ciphertext_one = PlainData::new(&[1, 2, 3, 4]);
-        let ciphertext_two = PlainData::new(&[4, 3, 2, 1]);
-        subject.add_string_rows(vec![
-            ("unencrypted_one", false),
-            ("unencrypted_two", false),
-            ("encrypted_one", true),
-            ("encrypted_two", true),
-        ]);
-        subject.change_password(None, old_password).unwrap();
-        subject
-            .set_string("unencrypted_one", cleartext_one)
-            .unwrap();
-        subject
-            .set_string("unencrypted_two", cleartext_two)
-            .unwrap();
-        subject
-            .set_bytes_e("encrypted_one", &ciphertext_one, old_password)
-            .unwrap();
-        subject
-            .set_bytes_e("encrypted_two", &ciphertext_two, old_password)
-            .unwrap();
-
-        subject
-            .change_password(Some(old_password), new_password)
-            .unwrap();
-
-        assert!(!subject.check_password(old_password).unwrap());
-        assert!(subject.check_password(new_password).unwrap());
-        assert_eq!(
-            subject.get_string("unencrypted_one"),
-            Ok(cleartext_one.to_string())
-        );
-        assert_eq!(
-            subject.get_string("unencrypted_two"),
-            Ok(cleartext_two.to_string())
-        );
-        assert_eq!(
-            subject.get_bytes_e("encrypted_one", new_password),
-            Ok(ciphertext_one)
-        );
-        assert_eq!(
-            subject.get_bytes_e("encrypted_two", new_password),
-            Ok(ciphertext_two)
-        );
+        let result = subject.get("schema_version").unwrap();
+        assert_eq!(result, ConfigDaoRecord::new ("schema_version", None, false));
     }
 }
-*/
