212c5e01df2841c1a3e2adbb0e97c3059a05c8c9
Dan Wiebe <dnwiebe@gmail.com>
2021-01-16 07:38:52 -0500

    GH-370: main.rs now uses PMP

--- automap/src/main.rs
+++ automap/src/main.rs
@@ -1,11 +1,12 @@
 // Copyright (c) 2019-2021, MASQ (https://masq.ai) and/or its affiliates. All rights reserved.
 
-use std::net::{IpAddr, UdpSocket, SocketAddr};
+use std::net::{IpAddr, UdpSocket, SocketAddr, Ipv4Addr};
 use std::str::FromStr;
 use automap_lib::protocols::pcp::map_packet::{Protocol, MapOpcodeData};
-use automap_lib::protocols::pcp::pcp_packet::{Opcode, PcpPacket};
 use automap_lib::protocols::utils::{Direction, Packet};
 use std::convert::TryFrom;
+use automap_lib::protocols::pmp::pmp_packet::{PmpPacket, Opcode};
+use automap_lib::protocols::pmp::get_packet::GetOpcodeData;
 
 pub fn main() {
     let args = std::env::args().collect::<Vec<String>>();
@@ -18,27 +19,20 @@ pub fn main() {
         Err(e) => abort (&format!("'{}' is not a properly-formatted IP address: {:?}", ip_string, e)),
     };
     let router_address = SocketAddr::new (router_ip, 5351);
-    let local_ip = IpAddr::from_str (&local_ipaddress::get().unwrap()).unwrap();
+    let local_ip = IpAddr::V4(Ipv4Addr::new (0, 0, 0, 0)); //IpAddr::from_str (&local_ipaddress::get().unwrap()).unwrap();
     let local_address = SocketAddr::new (local_ip, 5350);
     let socket = UdpSocket::bind(local_address).unwrap();
 
     let mut buf = [0u8; 1100];
     let packet_len = {
-        let packet = PcpPacket {
+        let packet = PmpPacket {
             direction: Direction::Request,
-            opcode: Opcode::Map,
+            opcode: Opcode::Get,
             result_code_opt: None,
-            lifetime: 1,
-            client_ip_opt: Some(local_ip),
-            epoch_time_opt: None,
-            opcode_data: Box::new (MapOpcodeData {
-                mapping_nonce: [0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC],
-                protocol: Protocol::Udp,
-                internal_port: 50000,
-                external_port: 50000,
-                external_ip_address: IpAddr::from_str("0.0.0.0").unwrap(),
-            }),
-            options: vec![]
+            opcode_data: Box::new(GetOpcodeData {
+                epoch_opt: None,
+                external_ip_address_opt: None
+            })
         };
         packet.marshal(&mut buf).unwrap()
     };
@@ -47,15 +41,16 @@ pub fn main() {
     let packet_len = socket.recv_from (&mut buf).unwrap().0;
 
     let buf_slice = &buf[0..packet_len];
-    let packet = PcpPacket::try_from (buf_slice).unwrap();
+    let packet = PmpPacket::try_from (buf_slice).unwrap();
     let mut tracker = Tracker::new();
     if packet.direction != Direction::Response {tracker.fail (format!("Response packet was a request, not a response"))};
-    if packet.opcode != Opcode::Other(0xFF) {tracker.fail (format!("Response packet opcode was {:?}, not Other(255)", packet.opcode))};
-    if packet.result_code_opt != Some (4) {tracker.fail (format!("Response packet result code was {:?}, not UNSUPP_OPCODE (4)", packet.result_code_opt))};
+    if packet.opcode != Opcode::Get {tracker.fail (format! ("Response packet opcode was {:?}, not Get", packet.opcode))};
+    if packet.result_code_opt != Some (0) {tracker.fail (format!("Response packet result code was {:?}, not 0", packet.result_code_opt))};
 
+    let opcode_data = packet.opcode_data.as_any().downcast_ref::<GetOpcodeData>().unwrap();
     println! ("Items of interest:");
-    println! ("Error lifetime: {} seconds (expected 1800, but not an error if different)", packet.lifetime);
-    println! ("Epoch time: {:?}", packet.epoch_time_opt);
+    println! ("Public IP address: {:?}", opcode_data.external_ip_address_opt);
+    println! ("Epoch time: {:?}", opcode_data.epoch_opt);
 
     tracker.resolve();
 }
