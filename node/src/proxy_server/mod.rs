ff5232f403c4585efe2967143b47e458bb9da8fe
FinsaasGH <Odinallfather123>
2021-08-31 11:57:37 -0600

    GH-446: Removed :? from proxy_server\mod.rs

--- node/src/proxy_server/mod.rs
+++ node/src/proxy_server/mod.rs
@@ -532,7 +532,7 @@ impl ProxyServer {
                                 Err(e) => {
                                     error!(
                                         logger,
-                                        "Neighborhood refused to answer route request: {:?}", e
+                                        "Neighborhood refused to answer route request: {}", e
                                     );
                                 }
                             };
