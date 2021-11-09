89889d6edfa9ef909e77bd6d4be26ebf192770aa
Bert <65427484+bertllll@users.noreply.github.com>
2021-11-09 02:28:42 +0100

    GH-503: corrected error processing to be less ugly

--- node/src/blockchain/dual_secret.rs
+++ node/src/blockchain/dual_secret.rs
@@ -30,10 +30,7 @@ impl TryFrom<&[u8]> for DualSecret {
                 ethsign_secret,
                 secp256k1_secret,
             }),
-            tuple => Err(resolve_hetero_errors(
-                tuple,
-                ("ethsign SecretKey: ", "secp256k1 SecretKey: "),
-            )),
+            tuple => Err(resolve_hetero_err(tuple)),
         }
     }
 }
@@ -48,61 +45,84 @@ impl From<(SecretKey, secp256k1::key::SecretKey)> for DualSecret {
     }
 }
 
-fn resolve_hetero_errors<T, E, R, S>(
-    to_check: (Result<T, E>, Result<R, S>),
-    intros: (&str, &str),
+fn resolve_hetero_err<T: Debug>(
+    to_check: (
+        Result<ethsign::SecretKey, T>,
+        Result<secp256k1::SecretKey, secp256k1::Error>,
+    ),
 ) -> String
 where
-    T: Debug,
-    E: Debug,
-    R: Debug,
-    S: Debug,
 {
-    fn if_err_convert_into_string(
-        is_err: bool,
-        r: Result<impl Debug, impl Debug>,
-        intro: &str,
-    ) -> String {
+    const SECRET_KEY: &str = "SecretKey";
+    fn convert_into_string<C: ?Sized>(is_err: bool, closure: Box<C>) -> String
+    where
+        C: FnOnce() -> String,
+    {
         if is_err {
-            let r = r.expect_err("wasn't err?");
-            format!("{}{:?}", intro, r)
+            closure()
         } else {
             String::new()
         }
     }
     let (a, b) = to_check;
-    let (intro_a, intro_b) = intros;
-    let a_is_err = a.is_err();
-    let b_is_err = b.is_err();
-    let a = if_err_convert_into_string(a_is_err, a, intro_a);
-    let b = if_err_convert_into_string(b_is_err, b, intro_b);
-    [a, b].iter().filter(|item| !item.is_empty()).join(", ")
+    let vec = vec![
+        (
+            a.is_err(),
+            Box::new(|| {
+                let e = a.expect_err("wasn't err?");
+                format!("ethsign {}: {:?}", SECRET_KEY, e)
+            }) as Box<dyn FnOnce() -> String>,
+        ),
+        (
+            b.is_err(),
+            Box::new(|| {
+                let e = b.expect_err("wasn't err?");
+                format!("secp256k1 {}: {}", SECRET_KEY, e)
+            }) as Box<dyn FnOnce() -> String>,
+        ),
+    ];
+    let summary = vec
+        .into_iter()
+        .map(|(is_err, closure)| convert_into_string(is_err, closure))
+        .filter(|item| !item.is_empty())
+        .join("; ");
+    summary
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::blockchain::dual_secret::{resolve_hetero_errors, DualSecret};
-    use masq_lib::utils::localhost;
+    use crate::blockchain::dual_secret::{resolve_hetero_err, DualSecret};
     use std::convert::TryFrom;
-    use std::net::IpAddr;
+
+    #[derive(Debug)]
+    struct Debugable;
 
     #[test]
     fn resolve_hetero_errors_catches_left_only() {
-        let results_for_left: (Result<u8, ()>, Result<&str, IpAddr>) = (Err(()), Ok("phew"));
+        let debugable = Debugable;
+        let results_for_left = (
+            Err(debugable),
+            Ok(secp256k1::SecretKey::from_slice(b"000000000000000000000000000000aa").unwrap()),
+        );
 
-        let output = resolve_hetero_errors(results_for_left, ("left ", "right "));
+        let output = resolve_hetero_err(results_for_left);
 
-        assert_eq!(output, "left ()".to_string())
+        assert_eq!(output, "ethsign SecretKey: Debugable".to_string())
     }
 
     #[test]
     fn resolve_hetero_errors_catches_right_only() {
-        let results_for_right: (Result<u8, ()>, Result<&str, IpAddr>) =
-            (Ok(2_u8), Err(localhost()));
+        let results_for_right: (Result<_, Debugable>, Result<_, _>) = (
+            Ok(ethsign::SecretKey::from_raw(b"000000000000000000000000000000ab").unwrap()),
+            Err(secp256k1::Error::InvalidSecretKey),
+        );
 
-        let output = resolve_hetero_errors(results_for_right, ("left ", "right "));
+        let output = resolve_hetero_err(results_for_right);
 
-        assert_eq!(output, "right 127.0.0.1".to_string());
+        assert_eq!(
+            output,
+            "secp256k1 SecretKey: secp: malformed or out-of-range secret key".to_string()
+        );
     }
 
     #[test]
@@ -115,7 +135,7 @@ mod tests {
         };
         assert_eq!(
             err,
-            "ethsign SecretKey: InvalidInputLength, secp256k1 SecretKey: InvalidSecretKey"
+            "ethsign SecretKey: InvalidInputLength; secp256k1 SecretKey: secp: malformed or out-of-range secret key"
                 .to_string()
         )
     }
