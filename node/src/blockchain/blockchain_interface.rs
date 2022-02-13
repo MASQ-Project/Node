59006a4cd8095b3be77ddb0c41338215e8d10e5c
KauriHero <55721017+kauri-hero@users.noreply.github.com>
2022-02-13 18:20:26 +1300

    remove comment line
    playing with format run to avoid exitcode

--- node/src/blockchain/blockchain_interface.rs
+++ node/src/blockchain/blockchain_interface.rs
@@ -1250,7 +1250,6 @@ mod tests {
     fn non_clandestine_signing_a_transaction_works_for_polygon_mumbai() {
         let chain = Chain::PolyMumbai;
         let nonce = 5; //must stay like this!
-        // signed_transaction_data changed after we changed the contract address of polygon matic
         let signed_transaction_data = "f8ad05850ba43b740083011980949b27034acabd44223fb23d628ba4849867ce1db280b844a9059cbb0000000000000000000000007788df76bbd9a0c7c3e5bf0f77bb28c60a167a7b000000000000000000000000000000000000000000000000000000e8d4a5100083027126a09fdbbd7064d3b7240f5422b2164aaa13d62f0946a683d82ee26f97f242570d90a077b49dbb408c20d73e0666ba0a77ac888bf7a9cb14824a5f35c97217b9bc0a5a";
         let in_bytes = decode_hex(signed_transaction_data).unwrap();
 
