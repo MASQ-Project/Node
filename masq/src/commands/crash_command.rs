58e8590bf12a4aa18e3f2acf9d693eaed99ecdcc
utkarshg6 <utkarshabhinav.torres@gmail.com>
2022-03-02 12:21:05 +0530

    GH-569: feat - make and test constants in masq/src/commands/crash_command

--- masq/src/commands/crash_command.rs
+++ masq/src/commands/crash_command.rs
@@ -12,30 +12,48 @@ pub struct CrashCommand {
     panic_message: String,
 }
 
+const CRASH_SUBCOMMAND: &str = "crash";
+const CRASH_COMMAND_ABOUT: &str = "Causes an element of the Node to crash with a specified message. Only valid if the Node has been started with '--crash-point message'";
+
+const ACTOR_ARG_NAME: &str = "actor";
+const ACTOR_ARG_HELP: &str = "Name of actor inside the Node that should be made to crash";
+const ACTOR_ARG_INDEX: u64 = 1;
+const ACTOR_ARG_POSSIBLE_VALUES: [&str; 5] = [
+    "BlockchainBridge",
+    "Dispatcher",
+    "Accountant",
+    "Configurator",
+    // "Hopper",
+    "Neighborhood",
+    // "ProxyClient",
+    // "ProxyServer",
+    // "UiGateway", // This should be the default, when it comes in
+    // "StreamHandlerPool",
+];
+const ACTOR_ARG_CASE_INSENSITIVE: bool = true;
+const ACTOR_ARG_DEFAULT_VALUE: &str = "BlockchainBridge";
+
+const MESSAGE_ARG_NAME: &str = "message";
+const MESSAGE_ARG_HELP: &str = "Panic message that should be produced by the crash";
+const MESSAGE_ARG_INDEX: u64 = 2;
+const MESSAGE_ARG_DEFAULT_VALUE: &str = "Intentional crash";
+
 pub fn crash_subcommand() -> App<'static, 'static> {
-    SubCommand::with_name("crash")
-        .about("Causes an element of the Node to crash with a specified message. Only valid if the Node has been started with '--crash-point message'")
-        .arg(Arg::with_name ("actor")
-            .help ("Name of actor inside the Node that should be made to crash")
-            .index (1)
-            .possible_values(&[
-                "BlockchainBridge",
-                "Dispatcher",
-                "Accountant",
-                "Configurator",
-                // "Hopper",
-                "Neighborhood",
-                // "ProxyClient",
-                // "ProxyServer",
-                // "UiGateway", // This should be the default, when it comes in
-                // "StreamHandlerPool",
-            ])
-            .case_insensitive(true)
-            .default_value("BlockchainBridge"))
-        .arg(Arg::with_name ("message")
-            .help ("Panic message that should be produced by the crash")
-            .index (2)
-            .default_value("Intentional crash")
+    SubCommand::with_name(CRASH_SUBCOMMAND)
+        .about(CRASH_COMMAND_ABOUT)
+        .arg(
+            Arg::with_name(ACTOR_ARG_NAME)
+                .help(ACTOR_ARG_HELP)
+                .index(ACTOR_ARG_INDEX)
+                .possible_values(&ACTOR_ARG_POSSIBLE_VALUES)
+                .case_insensitive(ACTOR_ARG_CASE_INSENSITIVE)
+                .default_value(ACTOR_ARG_DEFAULT_VALUE),
+        )
+        .arg(
+            Arg::with_name(MESSAGE_ARG_NAME)
+                .help(MESSAGE_ARG_HELP)
+                .index(MESSAGE_ARG_INDEX)
+                .default_value(MESSAGE_ARG_DEFAULT_VALUE),
         )
 }
 
@@ -61,12 +79,18 @@ impl CrashCommand {
         };
         Ok(Self {
             actor: matches
-                .value_of("actor")
-                .expect("actor parameter is not properly defaulted")
+                .value_of(ACTOR_ARG_NAME)
+                .expect(&*format!(
+                    "{} parameter is not properly defaulted",
+                    ACTOR_ARG_NAME
+                ))
                 .to_uppercase(),
             panic_message: matches
-                .value_of("message")
-                .expect("message parameter is not properly defaulted")
+                .value_of(MESSAGE_ARG_NAME)
+                .expect(&*format!(
+                    "{} parameter is not properly defaulted",
+                    MESSAGE_ARG_NAME
+                ))
                 .to_string(),
         })
     }
@@ -80,6 +104,37 @@ mod tests {
     use crate::test_utils::mocks::CommandContextMock;
     use masq_lib::messages::ToMessageBody;
     use std::sync::{Arc, Mutex};
+
+    #[test]
+    fn constants_have_correct_values() {
+        assert_eq!(CRASH_SUBCOMMAND, "crash");
+        assert_eq!(CRASH_COMMAND_ABOUT, "Causes an element of the Node to crash with a specified message. Only valid if the Node has been started with '--crash-point message'");
+        assert_eq!(ACTOR_ARG_NAME, "actor");
+        assert_eq!(
+            ACTOR_ARG_HELP,
+            "Name of actor inside the Node that should be made to crash"
+        );
+        assert_eq!(ACTOR_ARG_INDEX, 1);
+        assert_eq!(
+            ACTOR_ARG_POSSIBLE_VALUES,
+            [
+                "BlockchainBridge",
+                "Dispatcher",
+                "Accountant",
+                "Configurator",
+                "Neighborhood",
+            ]
+        );
+        assert_eq!(ACTOR_ARG_CASE_INSENSITIVE, true);
+        assert_eq!(ACTOR_ARG_DEFAULT_VALUE, "BlockchainBridge");
+        assert_eq!(MESSAGE_ARG_NAME, "message");
+        assert_eq!(
+            MESSAGE_ARG_HELP,
+            "Panic message that should be produced by the crash"
+        );
+        assert_eq!(MESSAGE_ARG_INDEX, 2);
+        assert_eq!(MESSAGE_ARG_DEFAULT_VALUE, "Intentional crash");
+    }
 
     #[test]
     fn testing_command_factory_here() {
