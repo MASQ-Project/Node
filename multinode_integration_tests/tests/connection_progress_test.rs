de30d0189fbd384b8a3d6666d35632da47692051
Dan Wiebe <dnwiebe@gmail.com>
2022-07-15 17:37:25 -0400

    Added multinode test

---
+++ multinode_integration_tests/tests/connection_progress_test.rs
@@ -0,0 +1,61 @@
+// Copyright (c) 2019, MASQ (https://masq.ai) and/or its affiliates. All rights reserved.
+
+use std::convert::TryInto;
+use std::time::Duration;
+
+use masq_lib::messages::{FromMessageBody, UiConnectionChangeBroadcast, UiConnectionChangeStage};
+use masq_lib::utils::find_free_port;
+use multinode_integration_tests_lib::masq_node::MASQNode;
+use multinode_integration_tests_lib::masq_node_cluster::MASQNodeCluster;
+use multinode_integration_tests_lib::masq_real_node::NodeStartupConfigBuilder;
+use node_lib::neighborhood::AccessibleGossipRecord;
+use node_lib::sub_lib::cryptde::PublicKey;
+
+#[test]
+fn connection_progress_is_properly_broadcast() {
+    let ui_port = find_free_port();
+    let mut cluster = MASQNodeCluster::start().unwrap();
+    // Set up small preexisting network that is much too small to route
+    let relay_2 = cluster.start_real_node(
+        NodeStartupConfigBuilder::standard()
+            .build()
+    );
+    let relay_1 = cluster.start_real_node(
+        NodeStartupConfigBuilder::standard()
+            .neighbor (relay_2.node_reference())
+            .build()
+    );
+    // Set up Node from which we will get connection-progress information
+    // and hook a UI to it
+    let subject = cluster.start_real_node(
+        NodeStartupConfigBuilder::standard()
+            .neighbor (relay_1.node_reference())
+            .ui_port(ui_port)
+            .build()
+    );
+    let ui_client = subject.make_ui(ui_port);
+
+    // Hook up an exit Node to make the Node fully connected
+    let _exit_node = cluster.start_real_node(
+        NodeStartupConfigBuilder::standard()
+            .neighbor (relay_2.node_reference())
+            .build()
+    );
+
+    let message_body = ui_client.wait_for_specific_broadcast(
+        vec!["connectionChange"],
+        Duration::from_secs(5)
+    );
+    let (ccb, _) = UiConnectionChangeBroadcast::fmb(message_body).unwrap();
+    if ccb.stage == UiConnectionChangeStage::ConnectedToNeighbor {
+        let message_body = ui_client.wait_for_specific_broadcast(
+            vec!["connectionChange"],
+            Duration::from_secs(5),
+        );
+        let (ccb, _) = UiConnectionChangeBroadcast::fmb(message_body).unwrap();
+        assert_eq! (ccb.stage, UiConnectionChangeStage::ThreeHopsRouteFound);
+    }
+    else {
+        assert_eq! (ccb.stage, UiConnectionChangeStage::ThreeHopsRouteFound);
+    }
+}
