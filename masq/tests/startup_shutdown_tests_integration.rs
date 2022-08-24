174f728b74b52d5dcdec572a2786fdb20eedda8d
Dan Wiebe <dnwiebe@gmail.com>
2022-08-24 07:21:10 -0400

    handles_startup_and_shutdown_integration now doesn't run in Actions on Windows

--- masq/tests/startup_shutdown_tests_integration.rs
+++ masq/tests/startup_shutdown_tests_integration.rs
@@ -94,7 +94,7 @@ fn masq_terminates_based_on_loss_of_connection_to_the_daemon_integration() {
 #[test]
 fn handles_startup_and_shutdown_integration() {
     if cfg!(windows) && is_running_under_github_actions() {
-        eprintln! ("This test is not run in Actions under Windows, because it's flaky there.")
+        eprintln!("This test is not run in Actions under Windows, because it's flaky there.")
     }
     let dir_path = ensure_node_home_directory_exists(
         "masq_integration_tests",
