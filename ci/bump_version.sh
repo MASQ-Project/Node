512b1305aa7605e8f3ad67083708c96606a0a50f
Utkarsh Gupta <utkarshabhinav.torres@gmail.com>
2023-03-16 11:51:32 +0530

    GH-679: check the pattern with grep

--- ci/bump_version.sh
+++ ci/bump_version.sh
@@ -25,15 +25,23 @@ declare -a failed_crates
 bump_version() {
   pushd "$CI_DIR/../$1"
   # Catches every `version` that begins a line and doesn't end with a comma.
-  pattern='s/^version\s*=.*[^,]\s*$/version = "'"$version"'"/'
+  find_pattern='^version\s*=.*[^,]\s*$'
+  replace_pattern='s/'$find_pattern'/version = "'"$version"'"/'
 
-# Catches every `version` that begins a line and doesn't end with a comma.
-  sed -i "$pattern" "$file"
+  grep -q "$find_pattern" "$file" | sed -i "$replace_pattern" "$file"
+  exit_code="$?"
+  if [[ "$exit_code" != "0" ]]; then
+    echo "grep failed for crate $1"
+    final_exit_code=1
+    failed_crates+=($1)
+    return
+  fi
+
   cargo generate-lockfile
   exit_code="$?"
   if [[ "$exit_code" != "0" ]]; then
-      final_exit_code=1
-      failed_crates+=($1)
+    final_exit_code=1
+    failed_crates+=($1)
   fi
 
   popd
