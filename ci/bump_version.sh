a56729f08bd0a089fe780ab476ef111cf03c5514
Utkarsh Gupta <utkarshabhinav.torres@gmail.com>
2023-05-10 13:59:13 +0530

    GH-700: improve the script to update the version in a single run

--- ci/bump_version.sh
+++ ci/bump_version.sh
@@ -14,6 +14,8 @@ echo "Searching for crates..."
 
 crates=($(find . -type d -exec bash -c '[ -f "$0"/Cargo.toml ]' '{}' \; -print))
 
+#crates=(./automap ./dns_utility ./multinode_integration_tests ./masq_lib ./masq ./port_exposer ./node)
+
 if [[ "${#crates[@]}" == "0" ]]; then
   echo "No crates found."
   exit 1
@@ -26,31 +28,42 @@ final_exit_code=0
 declare -a grep_failures
 declare -a lockfile_failures
 
-bump_version() {
+find_and_replace() {
   # Catches every `version` that begins a line and doesn't end with a comma.
   find_pattern='^version\s*=.*[^,]\s*$'
   replace_pattern='s/'$find_pattern'/version = "'"$version"'"/'
 
   grep -q "$find_pattern" "$file" && sed -i "$replace_pattern" "$file"
-  exit_code="$?"
-  if [[ "$exit_code" != "0" ]]; then
+  exit_code=$?
+  if [[ $exit_code != 0 ]]; then
     final_exit_code=1
     grep_failures+=($1)
-    return
   fi
+}
 
+update_lockfile() {
   cargo update --workspace
-  exit_code="$?"
-  if [[ "$exit_code" != "0" ]]; then
+  exit_code=$?
+  if [[ $exit_code != 0 ]]; then
     final_exit_code=1
     lockfile_failures+=($1)
   fi
+
+  return $exit_code
 }
 
 for crate in "${crates[@]}"
 do
   pushd "$crate"
-  bump_version "$crate"
+  find_and_replace "$crate"
+  popd
+done
+
+
+for crate in "${crates[@]}"
+do
+  pushd "$crate"
+  update_lockfile "$crate"
   popd
 done
 
