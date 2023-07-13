2ae19224de54aa49139942c0411c725b3c9eca75
Vojtěch Parkán <czarte@gmail.com>
2023-07-13 10:58:24 +0200

    resolved review comments on ensure_data_directory_has_specific_chain_directory_within_integration test

--- masq/tests/chain_specific_directory_test_integration.rs
+++ masq/tests/chain_specific_directory_test_integration.rs
@@ -11,23 +11,13 @@ mod utils;
 fn ensure_data_directory_has_specific_chain_directory_within_integration() {
     let port = find_free_port();
     let daemon_handle = DaemonProcess::new().start(port.clone());
-
     let masq_handle = MasqProcess::new().start_noninteractive(vec![
         "--ui-port",
         *&port.to_string().to_owned().as_str(),
         "setup",
     ]);
-
-    thread::sleep(Duration::from_millis(1000));
-
     let (stdout, _stderr, _exit_code) = masq_handle.stop();
-
-    if stdout.contains("MASQ/polygon-mainnet/MASQ/polygon-mainnet Default") {
-        panic!("Wrong directory: duplication of /MASQ/polygon-mainnet when Default");
-    }
-
     let mut masq_handle2 = MasqProcess::new().start_interactive(port, true);
-
     let mut stdin_handle = masq_handle2.create_stdin_handle();
 
     stdin_handle.type_command("setup --data-directory /home/booga/masqhome");
@@ -41,10 +31,9 @@ fn ensure_data_directory_has_specific_chain_directory_within_integration() {
         "{:29} {:64} {}",
         "data-directory", "/home/booga/masqhome/polygon-mainnet", "Set"
     );
-    if !stdout2.contains(&expected) {
-        println!("\nstdout: {} \n", stdout2);
-        panic!("Wrong directory: missing chain specific directory when Set");
-    }
+
+    assert!(!stdout.contains("MASQ/polygon-mainnet/MASQ/polygon-mainnet Default"), "Wrong directory: duplication of /MASQ/polygon-mainnet when Default");
+    assert!(stdout2.contains(&expected), "Wrong directory: missing chain specific directory when Set:\nstdout: {}\n", stdout2);
 
     daemon_handle.kill();
 }
