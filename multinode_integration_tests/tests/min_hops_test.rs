c897f0de0ad26e89a50aaadf80f6c1500510658b
Utkarsh Gupta <utkarshabhinav.torres@gmail.com>
2023-07-26 13:03:13 +0530

    GH-692: improve multinode test

--- multinode_integration_tests/tests/min_hops_test.rs
+++ multinode_integration_tests/tests/min_hops_test.rs
@@ -2,7 +2,8 @@
 
 use std::thread;
 use std::time::Duration;
-use masq_lib::utils::index_of;
+use masq_lib::messages::{ToMessageBody, UiSetConfigurationRequest};
+use masq_lib::utils::{find_free_port, index_of};
 use multinode_integration_tests_lib::masq_node::MASQNode;
 use multinode_integration_tests_lib::masq_node_cluster::MASQNodeCluster;
 use multinode_integration_tests_lib::masq_real_node::{make_consuming_wallet_info, MASQRealNode, NodeStartupConfigBuilder};
@@ -53,15 +54,18 @@ fn assert_http_end_to_end_routing_test(min_hops: Hops) {
 }
 
 #[test]
-fn make_linear_network() {
+fn min_hops_can_be_changed_during_runtime() {
     let hops = Hops::ThreeHops;
     let mut cluster = MASQNodeCluster::start().unwrap();
+    let ui_port = find_free_port();
     let first_node_config = NodeStartupConfigBuilder::standard()
         .min_hops(hops)
         .chain(cluster.chain)
         .consuming_wallet_info(make_consuming_wallet_info("first_node"))
+        .ui_port(ui_port)
         .build();
     let first_node = cluster.start_real_node(first_node_config);
+    let ui_client = first_node.make_ui(ui_port);
     let mut prev_node_reference = first_node.node_reference();
 
     for _ in 0..hops as u8 {
@@ -73,11 +77,28 @@ fn make_linear_network() {
         prev_node_reference = new_node.node_reference();
     }
 
-    thread::sleep(Duration::from_millis(500 * hops as u64));
+    thread::sleep(Duration::from_millis(5000));
 
     // first_node
 
     let mut client = first_node.make_client(8080, 5000);
+    client.send_chunk(b"GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n");
+    let response = client.wait_for_chunk();
+
+    assert_eq!(
+        index_of(&response, &b"<h1>Example Domain</h1>"[..]).is_some(),
+        true,
+        "Actual response:\n{}",
+        String::from_utf8(response).unwrap()
+    );
+
+    ui_client.send_request(UiSetConfigurationRequest {
+        name: "min-hops".to_string(),
+        value: Hops::FourHops.to_string(),
+    }.tmb(1));
+    let response = ui_client.wait_for_response(1, Duration::from_secs(2));
+    assert!(response.payload.is_ok());
+
     client.send_chunk(b"GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n");
     let response = client.wait_for_chunk();
 
