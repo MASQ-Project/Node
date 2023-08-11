9a54e6ff73ff7bd6b30a50d74ff6b1143db0c2d0
Syther007 <null.0.node@protonmail.com>
2023-08-11 20:50:14 +1200

    GH-651: Increased timeouts

--- multinode_integration_tests/tests/communication_failure_test.rs
+++ multinode_integration_tests/tests/communication_failure_test.rs
@@ -245,8 +245,8 @@ fn dns_resolution_failure_with_real_nodes() {
             )
         })
         .collect::<Vec<MASQRealNode>>();
-    thread::sleep(Duration::from_millis(500 * (nodes.len() as u64)));
-    let mut client = first_node.make_client(8080, 50000);
+    thread::sleep(Duration::from_millis(1000 * (nodes.len() as u64)));
+    let mut client = first_node.make_client(8080, 60000);
 
     client.send_chunk(b"GET / HTTP/1.1\r\nHost: www.nonexistent.com\r\n\r\n");
 
