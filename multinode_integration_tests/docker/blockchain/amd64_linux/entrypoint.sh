873a189051f177acba745ea6510e0418751d9ee8
Bert <65427484+bertllll@users.noreply.github.com>
2023-12-02 15:07:50 +0800

    GH-711: ganache-cli build script for two platforms

---
+++ multinode_integration_tests/docker/blockchain/amd64_linux/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# All ordinal wallets begin with zero balances. Except the contract owner wallet as the only one populated with usable
+# means. The means are meant to be redistributed from here to any accounts that will need it. Notice the argument
+# --account '<contract owner wallet private key>,<ETH in wei>' that assigns a certain initial balance.
+#
+# This same principle of wallets fed from this centric wallet at the test setup is followed by both the gas currency
+# and the MASQ tokens. With those, there is practically no other option for their strong dependency on the blockchain
+# smart contract that defines the entire supply is deployed to the contract owner's wallet.
+
+node /app/ganache-core.docker.cli.js \
+  -p 18545 \
+  --networkId 2 \
+  --verbose \
+  --mnemonic "timber cage wide hawk phone shaft pattern movie army dizzy hen tackle lamp absent write kind term toddler sphere ripple idle dragon curious hold" \
+  --defaultBalanceEther 0 \
+  --account '0xd4670b314ecb5e6b44b7fbe625ed746522c906316e66df31be64194ee6189188,10000000000000000000000'
--- multinode_integration_tests/docker/blockchain/build.sh
+++ multinode_integration_tests/docker/blockchain/build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash -evx
 # Copyright (c) 2019, MASQ (https://masq.ai) and/or its affiliates. All rights reserved.
 
-docker build -t ganache-cli .
+arch=`dpkg --print-architecture`
+
+if [[ $arch == "amd64" ]]; then
+
+    echo "Building ganache-cli image for linux/amd64 architecture"
+    docker build -t ganache-cli . -f amd64_linux/Dockerfile
+
+elif [[ $arch == "arm64" ]]; then
+
+    echo "Building ganache-cli image for linux/arm64 architecture"
+    docker build -t ganache-cli . -f arm64_linux/Dockerfile
+
+fi
