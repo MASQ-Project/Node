b9d07b33ce2da3534df4dbd7d979839944452f14
Bert <Bert@Bert.com>
2024-08-23 21:52:15 +0200

    GH-784-review-two-state: fixes that could work on Actions, appeasing it

--- ip_country/ci/all.sh
+++ ip_country/ci/all.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -xev
-# Copyright (c) 2019, MASQ (https://masq.ai) and/or its affiliates. All rights reserved.
+# Copyright (c) 2024, MASQ (https://masq.ai) and/or its affiliates. All rights reserved.
 CI_DIR="$( cd "$( dirname "$0" )" && pwd )"
-TOOLCHAIN_HOME="$1"
 
-#export RUSTC_WRAPPER="$HOME/.cargo/bin/sccache"
 pushd "$CI_DIR/.."
 ci/lint.sh
 ci/unit_tests.sh
-ci/integration_tests.sh "$TOOLCHAIN_HOME"
 popd
