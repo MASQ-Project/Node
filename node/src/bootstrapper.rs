acf99993f5e3cfc964c2c10b1514939d9d2f6f2a
Vojtěch Parkán <czarte@gmail.com>
2024-10-01 13:49:43 +0200

    clearing bootstrapper config

--- node/src/bootstrapper.rs
+++ node/src/bootstrapper.rs
@@ -402,7 +402,6 @@ impl BootstrapperConfig {
                 mode: NeighborhoodMode::ZeroHop,
                 min_hops: DEFAULT_MIN_HOPS,
                 exit_locations_opt: None,
-                fallback_routing: true,
             },
             when_pending_too_long_sec: DEFAULT_PENDING_TOO_LONG_SEC,
         }
@@ -1246,7 +1245,6 @@ mod tests {
             mode: NeighborhoodMode::OriginateOnly(vec![], rate_pack(9)),
             min_hops: MIN_HOPS_FOR_TEST,
             exit_locations_opt: None,
-            fallback_routing: true,
         };
         let earning_wallet = make_wallet("earning wallet");
         let consuming_wallet_opt = Some(make_wallet("consuming wallet"));
@@ -1868,7 +1866,6 @@ mod tests {
             ),
             min_hops: MIN_HOPS_FOR_TEST,
             exit_locations_opt: None,
-            fallback_routing: true,
         };
         config.data_directory = data_dir.clone();
         config.clandestine_port_opt = Some(port);
@@ -1940,7 +1937,6 @@ mod tests {
             ),
             min_hops: MIN_HOPS_FOR_TEST,
             exit_locations_opt: None,
-            fallback_routing: true,
         };
         config.data_directory = data_dir.clone();
         config.clandestine_port_opt = None;
@@ -1991,7 +1987,6 @@ mod tests {
             ),
             min_hops: MIN_HOPS_FOR_TEST,
             exit_locations_opt: None,
-            fallback_routing: true,
         };
         let listener_handler = ListenerHandlerNull::new(vec![]);
         let mut subject = BootstrapperBuilder::new()
@@ -2030,7 +2025,6 @@ mod tests {
             ))]),
             min_hops: MIN_HOPS_FOR_TEST,
             exit_locations_opt: None,
-            fallback_routing: true,
         };
         let listener_handler = ListenerHandlerNull::new(vec![]);
         let mut subject = BootstrapperBuilder::new()
@@ -2062,7 +2056,6 @@ mod tests {
             mode: NeighborhoodMode::ZeroHop,
             min_hops: MIN_HOPS_FOR_TEST,
             exit_locations_opt: None,
-            fallback_routing: true,
         };
         let listener_handler = ListenerHandlerNull::new(vec![]);
         let mut subject = BootstrapperBuilder::new()
@@ -2095,7 +2088,6 @@ mod tests {
                 mode: NeighborhoodMode::Standard(NodeAddr::default(), vec![], DEFAULT_RATE_PACK),
                 min_hops: MIN_HOPS_FOR_TEST,
                 exit_locations_opt: None,
-                fallback_routing: true,
             };
             let mut subject = BootstrapperBuilder::new().config(config).build();
             subject.set_up_clandestine_port();
