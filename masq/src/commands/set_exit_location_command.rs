5a234dffafce62291b3b049ea59ddd4b0ba3e84d
Vojtěch Parkán <czarte@gmail.com>
2024-10-09 12:20:36 +0200

    polishing helps for exit-location

--- masq/src/commands/set_exit_location_command.rs
+++ masq/src/commands/set_exit_location_command.rs
@@ -74,7 +74,10 @@ const EXIT_LOCATION_ABOUT: &str =
         masq> exit-location --country-codes \"CZ|SK\"                             // fallback-routing is OFF, \"CZ\" and \"SK\" countries has different prirority\n";
 
 // TODO update following help when GH-469 is done
-const COUNTRY_CODES_HELP: &str = "To obtain codes you cant use 'country-codes-list' command. You can specify country codes followingly:\n\n\
+const COUNTRY_CODES_HELP: &str = "Establish a set of countries that your Node should try to use for exit Nodes. You should choose from the countries that host the \
+        Nodes in your Neighborhood. List the countries in order of preference, separated by vertical pipes (|). If your level of preference \
+        for a group of countries is the same, separate those countries by commas (,).\n\
+        To obtain codes you cant use 'country-codes-list' command. You can specify country codes followingly:\n\n\
         masq> exit-location --country-codes \"CZ,PL|SK\"                          // \"CZ\" and \"PL\" countries has same priority \"1\", \"SK\" has prirority \"2\" \n\
         masq> exit-location --country-codes \"CZ|SK\"                             // \"CZ\" and \"SK\" countries has different prirority\n";
 
