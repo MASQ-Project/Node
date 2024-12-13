15b72539cee050e6b4855f910b5663f3fd4347b5
masqrauder <60554948+masqrauder@users.noreply.github.com>
2024-12-13 13:42:21 -0500

    GH-552: Cleanup migration tests

--- node/src/database/db_migrations/migrations/migration_8_to_9.rs
+++ node/src/database/db_migrations/migrations/migration_8_to_9.rs
@@ -45,25 +45,26 @@ mod tests {
 
         let result = subject.initialize_to_version(
             &dir_path,
+            8,
+            DbInitializationConfig::create_or_migrate(make_external_data()),
+        );
+
+        assert!(result.is_ok());
+
+        let result = subject.initialize_to_version(
+            &dir_path,
             9,
             DbInitializationConfig::create_or_migrate(make_external_data()),
         );
+
         let connection = result.unwrap();
         let (mp_value, mp_encrypted) = retrieve_config_row(connection.as_ref(), "max_block_count");
         let (cs_value, cs_encrypted) = retrieve_config_row(connection.as_ref(), "schema_version");
         assert_eq!(mp_value, None);
         assert_eq!(mp_encrypted, false);
-        assert_eq!(cs_value, Some("9".to_string()));
+        assert_eq!(cs_value, Some(9.to_string()));
         assert_eq!(cs_encrypted, false);
         TestLogHandler::new().assert_logs_contain_in_order(vec![
-            "DbMigrator: Database successfully migrated from version 0 to 1",
-            "DbMigrator: Database successfully migrated from version 1 to 2",
-            "DbMigrator: Database successfully migrated from version 2 to 3",
-            "DbMigrator: Database successfully migrated from version 3 to 4",
-            "DbMigrator: Database successfully migrated from version 4 to 5",
-            "DbMigrator: Database successfully migrated from version 5 to 6",
-            "DbMigrator: Database successfully migrated from version 6 to 7",
-            "DbMigrator: Database successfully migrated from version 7 to 8",
             "DbMigrator: Database successfully migrated from version 8 to 9",
         ]);
     }
