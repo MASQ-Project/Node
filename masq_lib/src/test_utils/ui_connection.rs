332a37adeb6aa85c11936170aa26c1b52df33b71
Dan Wiebe <dnwiebe@gmail.com>
2025-02-17 06:54:00 -0500

    Not very many errors left in websocket_supervisor.rs

--- masq_lib/src/test_utils/ui_connection.rs
+++ masq_lib/src/test_utils/ui_connection.rs
@@ -1,10 +1,11 @@
 // Copyright (c) 2019, MASQ (https://masq.ai) and/or its affiliates. All rights reserved.
 
 use crate::messages::{FromMessageBody, ToMessageBody, UiMessageError};
-use crate::test_utils::ui_connection::ReceiveResult::{Correct, MarshalError, TransactionError};
+use crate::ui_gateway::MessageBody;
+use crate::test_utils::ui_connection::ReceiveResult::{Correct, MBMarshalError, MarshalError, TransactionError};
 use crate::ui_gateway::MessagePath::Conversation;
 use crate::ui_gateway::MessageTarget::ClientId;
-use crate::ui_gateway::{MessagePath, NodeToUiMessage};
+use crate::ui_gateway::{MessagePath, MessageTarget, NodeToUiMessage};
 use crate::ui_traffic_converter::UiTrafficConverter;
 use crate::utils::localhost;
 use crate::websockets_types::{WSSender, WSReceiver};
@@ -12,11 +13,14 @@ use std::net::SocketAddr;
 use std::{fmt, io};
 use workflow_websocket::client::{Error, Message, WebSocket};
 use std::fmt::{Debug, Formatter};
+use std::time::Duration;
 use futures_util::io::{BufReader, BufWriter};
+use rustc_hex::ToHex;
 use soketto::{handshake, Data as SokettoDataType};
 use soketto::data::ByteSlice125;
 use soketto::handshake::ServerResponse;
 use tokio::net::TcpStream;
+use tokio::time::timeout;
 use tokio_util::compat::TokioAsyncReadCompatExt;
 
 pub struct UiConnection {
@@ -89,20 +93,20 @@ impl UiConnection {
         self.sender.flush().await.expect("Failed to flush pong");
     }
 
-    async fn receive(&mut self) -> (SokettoDataType, Vec<u8>) {
+    pub async fn receive(&mut self) -> Result<(SokettoDataType, Vec<u8>), soketto::connection::Error> {
         let mut message = Vec::new();
-        let data_type = self.receiver.receive_data(&mut message).await.expect("Failed to receive message");
+        let data_type = self.receiver.receive_data(&mut message).await?;
         match data_type {
             SokettoDataType::Binary(_) => if message.as_slice() == b"EMPTY_QUEUE" {
                 panic!("The queue is empty; all messages are gone.")
             },
             _ => ()
         }
-        (data_type, message)
+        Ok((data_type, message))
     }
 
     pub async fn receive_data(&mut self) -> Vec<u8> {
-        let (data_type, message) = self.receive().await;
+        let (data_type, message) = self.receive().await.unwrap();
         if let SokettoDataType::Binary(_) = data_type {
             message
         }
@@ -112,7 +116,7 @@ impl UiConnection {
     }
 
     pub async fn receive_string(&mut self) -> String {
-        let (data_type, message) = self.receive().await;
+        let (data_type, message) = self.receive().await.unwrap();
         if let SokettoDataType::Text(_) = data_type {
             String::from_utf8(message).expect("Failed to convert message to string")
         }
@@ -121,35 +125,50 @@ impl UiConnection {
         }
     }
 
+    pub async fn receive_node_to_ui(&mut self, target: MessageTarget) -> NodeToUiMessage {
+        let incoming_msg_json = self.receive_string().await;
+        UiTrafficConverter::new_unmarshal_to_ui(&incoming_msg_json, target)
+            .unwrap_or_else(|_| panic!("Deserialization problem with: {}: ", &incoming_msg_json))
+    }
+
     pub async fn receive_message<T: FromMessageBody>(
         &mut self,
         context_id: Option<u64>,
     ) -> ReceiveResult<T> {
         let incoming_msg_json = self.receive_string().await;
-        let incoming_msg = UiTrafficConverter::new_unmarshal_to_ui(&incoming_msg_json, ClientId(0))
+        let message_body = UiTrafficConverter::new_unmarshal(&incoming_msg_json)
             .unwrap_or_else(|_| panic!("Deserialization problem with: {}: ", &incoming_msg_json));
         if let Some(testing_id) = context_id {
-            match incoming_msg.body.path {
+            match message_body.path {
                 Conversation(id) if id == testing_id => (),
                 Conversation(id) if id != testing_id => panic!(
-                    "Context ID of the request and the response don't match; message: \
-         {:?}, request id: {}, response id: {}",
-                    incoming_msg, testing_id, id
+                    "Context ID of the request and the response don't match; message: {:?}, request id: {}, response id: {}",
+                    message_body, testing_id, id
                 ),
                 _ => (),
             }
         }
 
-        let result: Result<(T, u64), UiMessageError> = T::fmb(incoming_msg.body.clone());
+        let result: Result<(T, u64), UiMessageError> = T::fmb(message_body.clone());
         match result {
-            Ok((payload, _)) => Correct(incoming_msg.body.path, payload),
+            Ok((payload, _)) => Correct(message_body.path, payload),
             Err(UiMessageError::PayloadError(message_body)) => {
                 let payload_error = message_body
                     .payload
                     .expect_err("PayloadError message body contained no payload error");
                 TransactionError(payload_error.0, payload_error.1)
             }
-            Err(e) => MarshalError(incoming_msg, e),
+            Err(e) => MBMarshalError(message_body, e),
+        }
+    }
+
+    pub async fn assert_nothing_waiting(&mut self, milliseconds: u64) {
+        let mut message: Vec<u8> = Vec::new();
+        let future = self.receiver.receive_data(&mut message);
+        match timeout(Duration::from_millis(milliseconds), future).await {
+            Ok(Ok(data_type)) => panic!("Expected nothing; found {:?} message: {}", data_type, message.to_hex::<String>()),
+            Ok(Err(e)) => panic!("Error verifying no waiting message: {}", e),
+            Err(_) => return,
         }
     }
 
@@ -194,6 +213,7 @@ impl UiConnection {
             match self.receive_message::<T>(None).await {
                 Correct(path, msg) => break Ok((path, msg)),
                 TransactionError(code, msg) => break Err((code, msg)),
+                MBMarshalError(_, _) => continue,
                 MarshalError(_, _) => continue,
             }
         }
@@ -227,6 +247,11 @@ impl UiConnection {
         match extended_result {
             Correct(path, msg) => Ok((path, msg)),
             TransactionError(code, msg) => Err((code, msg)),
+            MBMarshalError(msg, e) => {
+                // TODO: This msg is something that came in from outside; the rules say it's not
+                // allowed to panic us. We should probably log it and return an error.
+                panic!("Deserialization of {:?} ended up with err: {:?}", msg, e)
+            },
             MarshalError(msg, e) => {
                 // TODO: This msg is something that came in from outside; the rules say it's not
                 // allowed to panic us. We should probably log it and return an error.
@@ -236,8 +261,21 @@ impl UiConnection {
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub enum ReceiveResult<T> {
     Correct(MessagePath, T),
     TransactionError(u64, String),
+    MBMarshalError(MessageBody, UiMessageError),
     MarshalError(NodeToUiMessage, UiMessageError),
+}
+
+impl<T> ReceiveResult<T> {
+    pub fn correct(self) -> Result<T, String> {
+        match self {
+            Correct(_path, msg) => Ok(msg),
+            TransactionError(code, msg) => Err(format!("Transaction error: {}: {}", code, msg)),
+            MBMarshalError(msg, e) => Err(format!("Body marshal error: {:?}: {:?}", msg, e)),
+            MarshalError(msg, e) => Err(format!("Marshal error: {:?}: {:?}", msg, e)),
+        }
+    }
 }
