b9c56ce87523d890250789b03e0de770f830ca2b
utkarshg6 <utkarshabhinav.torres@gmail.com>
2025-04-11 15:59:02 +0530

    GH-608: add boilerplate code

--- node/src/accountant/db_access_objects/sent_payable_dao.rs
+++ node/src/accountant/db_access_objects/sent_payable_dao.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
 use std::time::SystemTime;
 use ethereum_types::H256;
 use web3::types::Address;
 use crate::blockchain::blockchain_interface::blockchain_interface_web3::lower_level_interface_web3::TxStatus;
+use crate::database::rusqlite_wrappers::ConnectionWrapper;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum SentPayableDaoError {
@@ -13,17 +15,13 @@ pub enum SentPayableDaoError {
     // ErrorMarkFailed(String),
 }
 
-#[derive(Debug)]
-pub struct TxIdentifiers {
-    // pub rowid_results: Vec<(u64, H256)>,
-    // pub no_rowid_results: Vec<H256>,
-}
+type TxIdentifiers = HashMap<H256, Option<u64>>;
 
 pub struct Tx {
     // GH-608: Perhaps TxReceipt could be a similar structure to be used
+    tx_hash: String,
     receiver_address: Address,
     amount: u128,
-    tx_hash: String,
     timestamp: SystemTime,
     gas_price_wei: u64,
     nonce: u32,
@@ -41,4 +39,89 @@ pub trait SentPayableDao {
     fn insert_new_records(&self, txs: Vec<Tx>) -> Result<(), SentPayableDaoError>;
     fn delete_records(&self, ids: &[u64]) -> Result<(), SentPayableDaoError>;
     fn change_statuses(&self, ids: &[StatusChange]) -> Result<(), SentPayableDaoError>;
+}
+
+#[derive(Debug)]
+pub struct SentPayableDaoReal<'a> {
+    conn: Box<dyn ConnectionWrapper + 'a>,
+}
+
+impl<'a> SentPayableDaoReal<'a> {
+    pub fn new(conn: Box<dyn ConnectionWrapper + 'a>) -> Self {
+        // TODO: GH-608: Figure out how to test this guy
+        Self { conn }
+    }
+}
+
+impl SentPayableDao for SentPayableDaoReal<'_> {
+    fn get_tx_identifiers(&self, hashes: &[H256]) -> TxIdentifiers {
+        todo!()
+    }
+
+    fn retrieve_pending_txs(&self) -> Vec<Tx> {
+        todo!()
+    }
+
+    fn retrieve_txs_to_retry(&self) -> Vec<Tx> {
+        todo!()
+    }
+
+    fn insert_new_records(&self, txs: Vec<Tx>) -> Result<(), SentPayableDaoError> {
+        todo!()
+    }
+
+    fn delete_records(&self, ids: &[u64]) -> Result<(), SentPayableDaoError> {
+        todo!()
+    }
+
+    fn change_statuses(&self, ids: &[StatusChange]) -> Result<(), SentPayableDaoError> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::accountant::db_access_objects::pending_payable_dao::{
+        PendingPayableDao, PendingPayableDaoReal,
+    };
+    use crate::accountant::db_access_objects::sent_payable_dao::{
+        SentPayableDao, SentPayableDaoReal, Tx,
+    };
+    use crate::database::db_initializer::{
+        DbInitializationConfig, DbInitializer, DbInitializerReal,
+    };
+    use masq_lib::test_utils::utils::ensure_node_home_directory_exists;
+
+    #[test]
+    fn insert_new_records_works() {
+        let home_dir =
+            ensure_node_home_directory_exists("sent_payable_dao", "insert_new_records_works");
+        let wrapped_conn = DbInitializerReal::default()
+            .initialize(&home_dir, DbInitializationConfig::test_default())
+            .unwrap();
+        // let tx = Tx {
+        //     tx_hash: "".to_string(),
+        //     receiver_address: Default::default(),
+        //     amount: 0,
+        //     timestamp: (),
+        //     gas_price_wei: 0,
+        //     nonce: 0,
+        // };
+        let subject = SentPayableDaoReal::new(wrapped_conn);
+
+        // let _ = subject.insert_new_records()
+        todo!("finish me first");
+    }
+
+    #[test]
+    fn get_tx_identifiers_works() {
+        let home_dir =
+            ensure_node_home_directory_exists("sent_payable_dao", "get_tx_identifiers_works");
+        let wrapped_conn = DbInitializerReal::default()
+            .initialize(&home_dir, DbInitializationConfig::test_default())
+            .unwrap();
+        let subject = SentPayableDaoReal::new(wrapped_conn);
+
+        todo!("first you'll have to write tests for the insertion method");
+    }
 }
