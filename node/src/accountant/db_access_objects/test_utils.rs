e655f8c0090528480d456a9094544785a08a60ca
utkarshg6 <utkarshabhinav.torres@gmail.com>
2025-06-03 12:19:46 +0530

    GH-631: introduce FailedTxBuilder and updated trait for FailedPayableDao

--- node/src/accountant/db_access_objects/test_utils.rs
+++ node/src/accountant/db_access_objects/test_utils.rs
@@ -1,14 +1,15 @@
 // Copyright (c) 2025, MASQ (https://masq.ai) and/or its affiliates. All rights reserved.
 #![cfg(test)]
 
-use crate::accountant::db_access_objects::sent_payable_dao::Tx;
+use crate::accountant::db_access_objects::sent_payable_dao::{Hash, Tx};
 use crate::accountant::db_access_objects::utils::current_unix_timestamp;
 use web3::types::{Address, H256};
+use crate::accountant::db_access_objects::failed_payable_dao::{FailedTx, FailureReason};
 use crate::blockchain::blockchain_interface::blockchain_interface_web3::lower_level_interface_web3::TransactionBlock;
 
 #[derive(Default)]
 pub struct TxBuilder {
-    hash_opt: Option<H256>,
+    hash_opt: Option<Hash>,
     receiver_address_opt: Option<Address>,
     amount_opt: Option<u128>,
     timestamp_opt: Option<i64>,
@@ -22,7 +23,7 @@ impl TxBuilder {
         Default::default()
     }
 
-    pub fn hash(mut self, hash: H256) -> Self {
+    pub fn hash(mut self, hash: Hash) -> Self {
         self.hash_opt = Some(hash);
         self
     }
@@ -41,6 +42,79 @@ impl TxBuilder {
             gas_price_wei: self.gas_price_wei_opt.unwrap_or_default(),
             nonce: self.nonce_opt.unwrap_or_default(),
             block_opt: self.block_opt,
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct FailedTxBuilder {
+    hash_opt: Option<Hash>,
+    receiver_address_opt: Option<Address>,
+    amount_opt: Option<u128>,
+    timestamp_opt: Option<i64>,
+    gas_price_wei_opt: Option<u128>,
+    nonce_opt: Option<u64>,
+    reason_opt: Option<FailureReason>,
+    checked_opt: Option<u8>,
+}
+
+impl FailedTxBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn hash(mut self, hash: Hash) -> Self {
+        self.hash_opt = Some(hash);
+        self
+    }
+
+    pub fn receiver_address(mut self, receiver_address: Address) -> Self {
+        self.receiver_address_opt = Some(receiver_address);
+        self
+    }
+
+    pub fn amount(mut self, amount: u128) -> Self {
+        self.amount_opt = Some(amount);
+        self
+    }
+
+    pub fn timestamp(mut self, timestamp: i64) -> Self {
+        self.timestamp_opt = Some(timestamp);
+        self
+    }
+
+    pub fn gas_price_wei(mut self, gas_price_wei: u128) -> Self {
+        self.gas_price_wei_opt = Some(gas_price_wei);
+        self
+    }
+
+    pub fn nonce(mut self, nonce: u64) -> Self {
+        self.nonce_opt = Some(nonce);
+        self
+    }
+
+    pub fn reason(mut self, reason: FailureReason) -> Self {
+        self.reason_opt = Some(reason);
+        self
+    }
+
+    pub fn checked(mut self, checked: u8) -> Self {
+        self.checked_opt = Some(checked);
+        self
+    }
+
+    pub fn build(self) -> FailedTx {
+        FailedTx {
+            hash: self.hash_opt.unwrap_or_default(),
+            receiver_address: self.receiver_address_opt.unwrap_or_default(),
+            amount: self.amount_opt.unwrap_or_default(),
+            timestamp: self.timestamp_opt.unwrap_or_default(),
+            gas_price_wei: self.gas_price_wei_opt.unwrap_or_default(),
+            nonce: self.nonce_opt.unwrap_or_default(),
+            reason: self
+                .reason_opt
+                .unwrap_or_else(|| FailureReason::PendingTooLong),
+            checked: self.checked_opt.unwrap_or_default(),
         }
     }
 }
