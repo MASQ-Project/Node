a109005766b7b6b3b835a90ee1881a5f09bc017b
utkarshg6 <utkarshabhinav.torres@gmail.com>
2025-07-28 09:57:23 +0530

    GH-605: cleanup in start_scan()

--- node/src/accountant/scanners/payable_scanner/start_scan.rs
+++ node/src/accountant/scanners/payable_scanner/start_scan.rs
@@ -1,8 +1,6 @@
-use crate::accountant::db_access_objects::failed_payable_dao::FailureRetrieveCondition;
 use crate::accountant::db_access_objects::failed_payable_dao::FailureRetrieveCondition::ByStatus;
 use crate::accountant::db_access_objects::failed_payable_dao::FailureStatus::RetryRequired;
 use crate::accountant::db_access_objects::payable_dao::PayableAccount;
-use crate::accountant::db_access_objects::payable_dao::PayableRetrieveCondition::ByAddresses;
 use crate::accountant::db_access_objects::utils::from_unix_timestamp;
 use crate::accountant::scanners::payable_scanner::PayableScanner;
 use crate::accountant::scanners::payable_scanner_extension::msgs::{
@@ -15,7 +13,6 @@ use crate::sub_lib::wallet::Wallet;
 use masq_lib::logger::Logger;
 use std::collections::BTreeSet;
 use std::time::SystemTime;
-use web3::types::Address;
 
 impl StartableScanner<ScanForNewPayables, QualifiedPayablesMessage> for PayableScanner {
     fn start_scan(
@@ -87,12 +84,9 @@ impl StartableScanner<ScanForRetryPayables, QualifiedPayablesMessage> for Payabl
 mod tests {
     use super::*;
     use crate::accountant::db_access_objects::failed_payable_dao::FailureReason::PendingTooLong;
-    use crate::accountant::db_access_objects::failed_payable_dao::{
-        FailedPayableDao, FailedTx, FailureReason, FailureStatus,
-    };
-    use crate::accountant::db_access_objects::payable_dao::PayableRetrieveCondition::ByAddresses;
+    use crate::accountant::db_access_objects::failed_payable_dao::FailureStatus;
     use crate::accountant::db_access_objects::payable_dao::{
-        PayableAccount, PayableDao, PayableRetrieveCondition,
+        PayableAccount, PayableRetrieveCondition,
     };
     use crate::accountant::db_access_objects::test_utils::FailedTxBuilder;
     use crate::accountant::scanners::payable_scanner::test_utils::PayableScannerBuilder;
@@ -101,10 +95,7 @@ mod tests {
     use crate::accountant::test_utils::{
         make_payable_account, FailedPayableDaoMock, PayableDaoMock,
     };
-    use crate::accountant::PendingPayableId;
-    use crate::blockchain::blockchain_bridge::PendingPayableFingerprint;
     use crate::blockchain::test_utils::make_tx_hash;
-    use crate::sub_lib::accountant::PaymentThresholds;
     use crate::test_utils::{make_paying_wallet, make_wallet};
     use actix::System;
     use masq_lib::logger::Logger;
@@ -118,20 +109,21 @@ mod tests {
         init_test_logging();
         let test_name = "start_scan_for_retry_works";
         let logger = Logger::new(test_name);
-        let failed_payables_retrieve_txs_params_arc = Arc::new(Mutex::new(vec![]));
+        let retrieve_txs_params_arc = Arc::new(Mutex::new(vec![]));
         let non_pending_payables_params_arc = Arc::new(Mutex::new(vec![]));
         let timestamp = SystemTime::now();
-        let client_id = 1234;
-        let context_id = 4321;
-        let tx_hash_1 = make_tx_hash(1);
-        let payable_amount = 42;
-        let payable_account = make_payable_account(payable_amount);
-        let receiver_address_1 = payable_account.wallet.address();
+        let consuming_wallet = make_paying_wallet(b"consuming");
+        let response_skeleton = ResponseSkeleton {
+            client_id: 1234,
+            context_id: 4321,
+        };
+        let payable_account_1 = make_payable_account(42);
+        let receiver_address_1 = payable_account_1.wallet.address();
         let receiever_wallet_2 = make_wallet("absent in payable dao");
         let receiver_address_2 = receiever_wallet_2.address();
         let failed_tx_1 = FailedTxBuilder::default()
             .nonce(1)
-            .hash(tx_hash_1)
+            .hash(make_tx_hash(1))
             .receiver_address(receiver_address_1)
             .reason(PendingTooLong)
             .status(RetryRequired)
@@ -144,62 +136,58 @@ mod tests {
             .status(RetryRequired)
             .build();
         let expected_addresses = BTreeSet::from([receiver_address_1, receiver_address_2]);
-        let consuming_wallet = make_paying_wallet(b"consuming");
         let failed_payable_dao = FailedPayableDaoMock::new()
-            .retrieve_txs_params(&failed_payables_retrieve_txs_params_arc)
+            .retrieve_txs_params(&retrieve_txs_params_arc)
             .retrieve_txs_result(vec![failed_tx_1.clone(), failed_tx_2.clone()]);
         let payable_dao = PayableDaoMock::new()
             .non_pending_payables_params(&non_pending_payables_params_arc)
-            .non_pending_payables_result(vec![payable_account.clone()]); // the failed_tx_2 is absent
+            .non_pending_payables_result(vec![payable_account_1.clone()]); // the second record is absent
         let mut subject = PayableScannerBuilder::new()
             .failed_payable_dao(failed_payable_dao)
             .payable_dao(payable_dao)
             .build();
-        let system = System::new(test_name);
 
         let result = Scanners::start_correct_payable_scanner::<ScanForRetryPayables>(
             &mut subject,
             &consuming_wallet,
             timestamp,
-            Some(ResponseSkeleton {
-                client_id,
-                context_id,
-            }),
+            Some(response_skeleton),
             &logger,
         );
 
-        System::current().stop();
         let scan_started_at = subject.scan_started_at();
-        let failed_payables_retrieve_txs_params =
-            failed_payables_retrieve_txs_params_arc.lock().unwrap();
+        let failed_payables_retrieve_txs_params = retrieve_txs_params_arc.lock().unwrap();
         let non_pending_payables_params = non_pending_payables_params_arc.lock().unwrap();
-        let mut new_payable_account = payable_account;
-        new_payable_account.balance_wei = new_payable_account.balance_wei + failed_tx_1.amount;
+        let expected_payables = {
+            let mut payables_vec = vec![];
+            let mut expected_payable_1 = payable_account_1;
+            expected_payable_1.balance_wei = expected_payable_1.balance_wei + failed_tx_1.amount;
+            payables_vec.push(QualifiedPayablesBeforeGasPriceSelection {
+                payable: expected_payable_1,
+                previous_attempt_gas_price_minor_opt: Some(failed_tx_1.gas_price_wei),
+            });
+
+            let expected_payable_2 = PayableAccount {
+                wallet: receiever_wallet_2,
+                balance_wei: failed_tx_2.amount,
+                last_paid_timestamp: from_unix_timestamp(failed_tx_2.timestamp),
+                pending_payable_opt: None,
+            };
+            payables_vec.push(QualifiedPayablesBeforeGasPriceSelection {
+                payable: expected_payable_2,
+                previous_attempt_gas_price_minor_opt: Some(failed_tx_2.gas_price_wei),
+            });
+
+            payables_vec
+        };
         assert_eq!(
             result,
             Ok(QualifiedPayablesMessage {
                 qualified_payables: UnpricedQualifiedPayables {
-                    payables: vec![
-                        QualifiedPayablesBeforeGasPriceSelection {
-                            payable: new_payable_account,
-                            previous_attempt_gas_price_minor_opt: Some(failed_tx_1.gas_price_wei),
-                        },
-                        QualifiedPayablesBeforeGasPriceSelection {
-                            payable: PayableAccount {
-                                wallet: receiever_wallet_2,
-                                balance_wei: failed_tx_2.amount,
-                                last_paid_timestamp: from_unix_timestamp(failed_tx_2.timestamp),
-                                pending_payable_opt: None,
-                            },
-                            previous_attempt_gas_price_minor_opt: Some(failed_tx_2.gas_price_wei),
-                        }
-                    ]
+                    payables: expected_payables
                 },
                 consuming_wallet: consuming_wallet.clone(),
-                response_skeleton_opt: Some(ResponseSkeleton {
-                    client_id,
-                    context_id,
-                }),
+                response_skeleton_opt: Some(response_skeleton),
             })
         );
         assert_eq!(scan_started_at, Some(timestamp));
