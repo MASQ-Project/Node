fe2804df12f443d3fc1e4340c15edc1ba2f3e1ed
Dan Wiebe <dnwiebe@gmail.com>
2025-08-14 21:53:17 -0400

    Fixed the AddRouteResultMessage panic problem too

--- node/src/sub_lib/ttl_hashmap.rs
+++ node/src/sub_lib/ttl_hashmap.rs
@@ -54,6 +54,17 @@ where
         }
     }
 
+    pub fn remove(&self, key: &K) -> Option<Rc<V>> {
+        self.remove_expired_entries();
+
+        match self.data.borrow_mut().remove(key) {
+            Some((result, _)) => {
+                Some(result)
+            }
+            None => None,
+        }
+    }
+
     fn remove_expired_entries(&self) {
         let now = Instant::now();
 
@@ -88,15 +99,53 @@ mod tests {
     fn new_sets_ttl() {
         let subject = TtlHashMap::<u32, u32>::new(Duration::from_millis(1000));
 
-        assert_eq!(subject.ttl, Duration::from_millis(1000));
+        let result = subject.ttl;
+
+        assert_eq!(result, Duration::from_millis(1000));
     }
 
     #[test]
-    fn remove_returns_none_for_entry_that_was_never_inserted() {
+    fn get_returns_none_for_entry_that_was_never_inserted() {
         let subject = TtlHashMap::<u32, u32>::new(Duration::from_millis(1000));
 
-        assert_eq!(subject.get(&11u32), None);
+        let result = subject.get(&11u32);
+
+        assert_eq!(result, None);
         assert_eq!(subject.ttl(), Duration::from_millis(1000));
+    }
+
+    #[test]
+    fn remove_returns_none_if_no_such_entry_exists() {
+        let subject = TtlHashMap::<u32, u32>::new(Duration::from_millis(1000));
+
+        let result = subject.remove(&11u32);
+
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn remove_returns_existing_entry_and_removes() {
+        let mut subject = TtlHashMap::<u32, u32>::new(Duration::from_millis(1000));
+        subject.insert(11u32, 42u32);
+
+        let before_result = subject.remove(&11u32);
+        let after_result = subject.remove(&11u32);
+
+        assert_eq!(before_result, Some(Rc::new(42u32)));
+        assert_eq!(after_result, None);
+    }
+
+    #[test]
+    fn ttl_hashmap_remove_removes_expired_entry() {
+        let mut subject = TtlHashMap::new(Duration::from_millis(10));
+        subject.insert(42u32, "Hello");
+        thread::sleep(Duration::from_millis(20));
+
+        let result = subject.remove(&11u32); // nonexistent key
+
+        assert_eq!(result, None);
+        // Low-level get, because high-level get would remove it if .remove() didn't
+        assert_eq!(subject.data.borrow().get(&42u32), None);
     }
 
     #[test]
@@ -114,20 +163,18 @@ mod tests {
     #[test]
     fn ttl_hashmap_get_removes_expired_entry() {
         let mut subject = TtlHashMap::new(Duration::from_millis(10));
-
         subject.insert(42u32, "Hello");
-
         thread::sleep(Duration::from_millis(20));
 
-        assert_eq!(subject.get(&42u32), None);
+        let result = subject.get(&42u32);
+
+        assert_eq!(result, None);
     }
 
     #[test]
     fn ttl_hashmap_insert_removes_expired_entry() {
         let mut subject = TtlHashMap::new(Duration::from_millis(10));
-
         subject.insert(42u32, "Hello");
-
         thread::sleep(Duration::from_millis(20));
 
         subject.insert(24u32, "World");
